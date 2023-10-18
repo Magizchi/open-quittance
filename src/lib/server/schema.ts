19823f4212c1c05b636372d8afd82ddc3e7af036
Rajanan <g.rajan1993@gmail.com>
2023-10-18 11:18:36 +0000

    fix(schema): fix schema and col names

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -3,30 +3,32 @@ import { mysqlTable, bigint, varchar, date } from 'drizzle-orm/mysql-core';
 export const ownersTable = mysqlTable('owners', {
     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
     name: varchar('name', { length: 255 }),
-    address: varchar('name', { length: 255 }),
+    address: varchar('address', { length: 255 }),
     city: varchar('city', { length: 255 }),
-    postalCode: varchar('postalCode', { length: 5 })
+    postalCode: varchar('postalCode', { length: 5 }),
+    email: varchar('email', { length: 255 }),
+    password: varchar('password', { length: 255 }),
+
 })
 
 export const tenantsTable = mysqlTable('tenants', {
     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
     name: varchar('name', { length: 255 }),
-    address: varchar('name', { length: 255 }),
+    address: varchar('address', { length: 255 }),
     city: varchar('city', { length: 255 }),
     postalCode: varchar('postalCode', { length: 5 }),
-    password: varchar('password', { length: 255 }),
-    email: varchar('email', { length: 255 })
+    email: varchar('email', { length: 255 }),
+    password: varchar('password', { length: 255 })
 })
 
 export const localsTable = mysqlTable('locals', {
     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
     name: varchar('name', { length: 255 }),
-    address: varchar('name', { length: 255 }),
+    address: varchar('address', { length: 255 }),
     city: varchar('city', { length: 255 }),
     postalCode: varchar('postalCode', { length: 5 }),
     ownerId: bigint('owner_id', { mode: 'number' }).references(() => ownersTable.id),
     tenantId: bigint('tenant_id', { mode: 'number' }).references(() => tenantsTable.id)
-
 })
 
 
