18cf7dace4c766851f0b64e38d399e59ed31f90f
Rajanan <g.rajan1993@gmail.com>
2023-10-18 10:03:00 +0000

    fix(server:database): fix import env variables

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,13 +1,15 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 import mysql from "mysql2/promise";
+import { DB_DATABASE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } from '$env/static/private';
+
 
 const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
 });
 
 const db = drizzle(connection);
