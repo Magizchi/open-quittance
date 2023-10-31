6a6d47717fa4c3d0ebd8307e1a61210e5655abaf
Rajanan <g.rajan1993@gmail.com>
2023-10-31 10:18:08 +0000

    feat(schema): rewrite bdd schema

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,25 +1,41 @@
-import { mysqlTable, bigint, varchar, date } from 'drizzle-orm/mysql-core';
+import { mysqlTable, bigint, varchar, date, int } from 'drizzle-orm/mysql-core';
 
-export const ownersTable = mysqlTable('owners', {
+export const usersTable = mysqlTable('users', {
     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
-    name: varchar('name', { length: 255 }),
-    address: varchar('address', { length: 255 }),
-    city: varchar('city', { length: 255 }),
-    postalCode: varchar('postalCode', { length: 5 }),
-    email: varchar('email', { length: 255 }),
-    password: varchar('password', { length: 255 }),
-
+    firstName: varchar('firstName', { length: 255 }),
+    lastName: varchar('firstName', { length: 255 }),
+    email: varchar('email', { length: 255 }).notNull(),
+    password: varchar('password', { length: 255 }).notNull(),
+    loginToken: varchar('loginToken', { length: 255 }),
 })
 
-export const tenantsTable = mysqlTable('tenants', {
+export const companiesTable = mysqlTable('companies', {
     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
-    name: varchar('name', { length: 255 }),
-    address: varchar('address', { length: 255 }),
-    city: varchar('city', { length: 255 }),
-    postalCode: varchar('postalCode', { length: 5 }),
-    email: varchar('email', { length: 255 }),
-    password: varchar('password', { length: 255 })
+    userId: bigint('user_id', { mode: 'number' }).references(() => usersTable.id),
+    companyName: varchar('name', { length: 255 }).notNull(),
+    siretNumber: int('siretNumber'),
+    address: varchar('address', { length: 255 }).notNull(),
+    city: varchar('city', { length: 255 }).notNull(),
+    postalCode: varchar('postalCode', { length: 5 }).notNull(),
 })
+
+// export const ownersTable = mysqlTable('owners', {
+//     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
+//     userId: bigint('user_id', { mode: 'number' }).references(() => usersTable.id),
+//     companieName: varchar('name', { length: 255 }),
+//     address: varchar('address', { length: 255 }),
+//     city: varchar('city', { length: 255 }),
+//     postalCode: varchar('postalCode', { length: 5 }),
+// })
+
+// export const tenantsTable = mysqlTable('tenants', {
+//     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
+//     userId: bigint('user_id', { mode: 'number' }).references(() => usersTable.id),
+//     companieName: varchar('name', { length: 255 }),
+//     address: varchar('address', { length: 255 }),
+//     city: varchar('city', { length: 255 }),
+//     postalCode: varchar('postalCode', { length: 5 }),
+// })
 
 export const localsTable = mysqlTable('locals', {
     id: bigint('id', { mode: 'number' }).primaryKey().autoincrement(),
@@ -27,8 +43,8 @@ export const localsTable = mysqlTable('locals', {
     address: varchar('address', { length: 255 }),
     city: varchar('city', { length: 255 }),
     postalCode: varchar('postalCode', { length: 5 }),
-    ownerId: bigint('owner_id', { mode: 'number' }).references(() => ownersTable.id),
-    tenantId: bigint('tenant_id', { mode: 'number' }).references(() => tenantsTable.id)
+    ownerId: bigint('owner_id', { mode: 'number' }).references(() => companiesTable.id),
+    tenantId: bigint('tenant_id', { mode: 'number' }).references(() => companiesTable.id)
 })
 
 
@@ -37,10 +53,6 @@ export const receiptsTable = mysqlTable('receipts', {
     paymentDate: date('paymentDate'),
     createAt: date('createAt'),
     period: varchar('period', { length: 255 }),
-    ownerId: bigint('owner_id', { mode: 'number' }).references(() => ownersTable.id),
-    tenantId: bigint('tenant_id', { mode: 'number' }).references(() => tenantsTable.id)
+    ownerId: bigint('owner_id', { mode: 'number' }).references(() => companiesTable.id),
+    tenantId: bigint('tenant_id', { mode: 'number' }).references(() => companiesTable.id)
 })
-
-// export const ownersTableRelations = relations(ownersTable, ({ many }) => ({ locals: many(localsTable) }))
-
-// export const localsTableRelations = relations(localsTable, ({ one }) => ({ owners: one(ownersTable, { fields: [localsTable.ownerId], references: [ownersTable.id] }) }))4
