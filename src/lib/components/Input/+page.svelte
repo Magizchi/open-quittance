87145f91091718a7398802d6bc714ba1f1a75d47
Rajanan <g.rajan1993@gmail.com>
2023-10-31 15:13:21 +0000

    feat(component:input) improve component

--- src/lib/components/Input/+page.svelte
+++ src/lib/components/Input/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	export let label: string;
 	export let name: string;
-	export let placeholder: string;
 </script>
 
 <div>
-	<label for={name} class="block mb-2 text-sm font-medium">{label}</label>
+	{#if label}
+		<label for={name} class="block mb-2 text-sm font-medium">{label}</label>
+	{/if}
 	<input
 		type="text"
 		id={name}
 		class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-		{placeholder}
-		{name}
+		{...$$restProps}
 	/>
 </div>
