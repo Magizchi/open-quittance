e4547495ccf7adc86d016948da612d54d0cb02a9
Rajanan <g.rajan1993@gmail.com>
2023-10-31 15:15:36 +0000

    refacto(default:app:server): add custom func checkToken,

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,27 +1,20 @@
 import { redirect } from '@sveltejs/kit';
-import jwt from 'jsonwebtoken';
-import { JWT_SECRET_TOKEN } from '$env/static/private';
 import db from '$lib/server/database.js';
-import { ownersTable } from '$lib/server/schema.js';
+import { usersTable } from '$lib/server/schema.js';
 import { eq } from 'drizzle-orm';
+import CheckToken from '$lib/utils/CheckToken'
 
-//JWT
 export const load = async ({ cookies }) => {
-    const cookiesJwt = cookies.get('auth')
-    if (!cookiesJwt) {
-        throw redirect(303, "/login")
-    }
+    const userInfo = CheckToken(cookies)
 
-    const decoded = jwt.verify(cookiesJwt, JWT_SECRET_TOKEN) as any;
-
-    const [user] = await db.select().from(ownersTable).where(eq(ownersTable.email, decoded.authToke.email))
+    const [user] = await db.select().from(usersTable).where(eq(usersTable.email, userInfo.email))
     if (!user) {
         throw redirect(303, '/')
     }
 
-    if (user.loginToken !== decoded.authToke.loginToken) {
+    if (user.loginToken !== userInfo.loginToken) {
         throw redirect(303, '/')
     }
-    return { email: user.email, name: user.name, id: user.id }
+    return { email: user.email, name: user.firstName, id: user.id }
 }
 
