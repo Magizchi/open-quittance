7da7ff0613db6be90a04cd34b0d2de00be52b239
Rajanan <g.rajan1993@gmail.com>
2023-10-31 15:14:37 +0000

    fix(main:layout:server): delete all

--- src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { migrate } from 'drizzle-orm/mysql2/migrator';
-import db from '../lib/server/database'
-import { redirect } from '@sveltejs/kit';
-
-
-
-//MIGRATION > this will automatically run needed migrations on the database
-await migrate(db, { migrationsFolder: './drizzle' });
-
