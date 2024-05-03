cdb3033e26e9e0751e4a1cfacab40e2266b1d46a
Rajanan <g.rajan1993@gmail.com>
2024-05-03 20:15:33 +0000

    feat(atom:Icon):add penIcon

--- src/lib/components/atoms/Icons/icon.ts
+++ src/lib/components/atoms/Icons/icon.ts
@@ -1,3 +1,4 @@
 export { default as UserIcon } from "./userIcon.svelte";
 export { default as KeyIcon } from "./keyIcon.svelte";
 export { default as PrintIcon } from "./PrintIcon.svelte";
+export { default as PenIcon } from "./PenIcon.svelte";
