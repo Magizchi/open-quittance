f6e57430d9aa5b1cc461aa0fd8a7a6603fbd4b04
Rajanan <g.rajan1993@gmail.com>
2024-05-03 20:18:07 +0000

    feat(page:landing):add validation receipt

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,11 +3,12 @@
 	import Table from '$lib/components/organisms/table.svelte';
 	import dayjs from 'dayjs';
 	import PrintIcon from '$lib/components/atoms/Icons/PrintIcon.svelte';
+	import { PenIcon } from '$lib/components/atoms/Icons/icon.js';
 	export let data;
 
 	let pdfBuffer: any;
 
-	async function getPdf(receiptId: number | string) {
+	async function getPdf(receiptId: number) {
 		const response = await fetch(`/api/generate-pdf?receiptId=${receiptId}`).then((data) =>
 			data.blob()
 		);
@@ -15,6 +16,18 @@
 
 		const blob = new Blob([pdfBuffer], { type: 'application/pdf' });
 		saveAs(blob, 'hello-world.pdf');
+	}
+
+	async function SetPaymentDate(receiptId: number) {
+		await fetch('/api/receipts', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			},
+			body: JSON.stringify({ receiptId })
+		}).then((data) => data.json());
+
+		// add modal
 	}
 
 	const columns = [
@@ -63,6 +76,9 @@
 				<td>
 					<button on:click={() => getPdf(row.id)}>
 						<PrintIcon width="25" class="m-3" />
+					</button>
+					<button disabled={row.paymentDate ? true : false} on:click={() => SetPaymentDate(row.id)}>
+						<PenIcon width="25" class="m-3" />
 					</button>
 				</td>
 			{/if}
