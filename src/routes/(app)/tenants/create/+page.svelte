291fb77de6c0006d2d8d5f3939b188bb10d9f639
Rajanan <g.rajan1993@gmail.com>
2024-05-07 13:29:48 +0000

    feat(page:create tenant): add form for create tenant

---
+++ src/routes/(app)/tenants/create/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import FormForUser from '$lib/components/organisms/Forms/createUser.svelte';
+	export let form;
+	console.log('form', form);
+</script>
+
+<div class="max-w-2xl p-16 mx-auto">
+	<FormForUser />
+</div>
