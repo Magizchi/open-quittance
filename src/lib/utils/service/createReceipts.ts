e14ff08f65eb9c2a25523a7dc7cba56b2b35a438
Rajanan <g.rajan1993@gmail.com>
2024-05-07 13:53:16 +0000

    feat(utils:rental):function to create receitpts

---
+++ src/lib/utils/service/createReceipts.ts
@@ -0,0 +1,42 @@
+import { receiptsTable } from '$lib/server/schema.js';
+import type { RentalModel } from '$lib/models/rental.model.ts';
+import dayjs from 'dayjs';
+import db from '$lib/server/database';
+
+/**
+ * CreateReceipt
+ * @param rental All info Tenant, Landlord, Property for create the receipt 
+ * @param startDate when then rental period start
+ * @param endDate when then rental period end
+ */
+export const CreateReceipts = async (rental: RentalModel, startDate: string, endDate: string) => {
+    const newReceipts = {
+        rental_id: rental.rentals!.id,
+        number: '',
+        landlord_id: rental.landlords!.id,
+        landlord_fullName: rental.landlords!.fullName,
+        landlord_address: rental.landlords!.address,
+        landlord_city: rental.landlords!.city,
+        landlord_postalCode: rental.landlords!.postalCode,
+
+        tenant_id: rental.tenants!.id,
+        tenant_address: rental.tenants!.address,
+        tenant_city: rental.tenants!.city,
+        tenant_fullName: rental.tenants!.fullName,
+        tenant_postalCode: rental.tenants!.postalCode,
+
+        rent: rental.properties!.rent,
+        condo_fees: rental.properties!.condo_fees,
+        taxes: rental.properties!.taxes,
+
+        property_address: rental.properties!.address,
+        property_city: rental.properties!.city,
+        property_postalCode: rental.properties!.postalCode,
+
+        paymentDate: null,
+        startDate: dayjs(startDate).toDate(),
+        endDate: dayjs(endDate).toDate(),
+        createAt: dayjs(dayjs().format('YYYY/MM/DD')).toDate()
+    };
+    return await db.insert(receiptsTable).values(newReceipts);
+};
