88cabf15022f5330b9ef89cccb6ea89f1d3e1308
Rajanan <g.rajan1993@gmail.com>
2024-05-07 09:28:28 +0000

    style(atom:input):change style

--- src/lib/components/atoms/Input.svelte
+++ src/lib/components/atoms/Input.svelte
@@ -16,7 +16,7 @@
 		id={name}
 		class={$$slots.CustomIcon
 			? 'w-full pl-1 border border-gray-200 rounded-r-lg outline-none focus:ring-1 ring-blue-400'
-			: 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}
+			: 'border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}
 		{name}
 		{...$$restProps}
 	/>
