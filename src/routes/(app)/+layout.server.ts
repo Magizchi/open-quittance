332b1e18ff628adee98732d61b5eb64fbec6b020
Rajanan <g.rajan1993@gmail.com>
2024-05-09 15:03:17 +0000

    refacto

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -2,19 +2,16 @@ import { redirect } from '@sveltejs/kit';
 import db from '$lib/server/database.js';
 import { usersTable } from '$lib/server/schema.js';
 import { eq } from 'drizzle-orm';
-import CheckToken from '$lib/utils/CheckToken'
+import CheckToken from '$lib/utils/CheckToken';
 
 export const load = async ({ cookies }) => {
-    const userInfo = CheckToken(cookies)
-
-    const [user] = await db.select().from(usersTable).where(eq(usersTable.email, userInfo.email))
+    const userInfo = CheckToken(cookies);
+    const [user] = await db.select().from(usersTable).where(eq(usersTable.email, userInfo.email));
     if (!user) {
-        throw redirect(303, '/')
+        throw redirect(303, '/');
     }
-
     if (user.loginToken !== userInfo.loginToken) {
-        throw redirect(303, '/')
+        throw redirect(303, '/');
     }
-    return { email: user.email, name: user.firstName, id: user.id }
-}
-
+    return { user };
+};
