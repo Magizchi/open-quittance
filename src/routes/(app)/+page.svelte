1e516788ba9f1e26957a95cdd140ebd32968d79f
Rajanan <g.rajan1993@gmail.com>
2024-05-20 10:03:58 +0000

    feat(page:ladding):add address to the table

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,7 +5,6 @@
 	import PrintIcon from '$lib/components/atoms/Icons/PrintIcon.svelte';
 	import { PenIcon } from '$lib/components/atoms/Icons/icon.js';
 	export let data;
-
 	let pdfBuffer: any;
 
 	async function getPdf(receiptId: number) {
@@ -44,12 +43,20 @@
 			dataIndex: 'property'
 		},
 		{
-			header: 'Ville',
-			dataIndex: 'city'
+			header: 'Adresse du local',
+			dataIndex: 'address'
 		},
 		{
 			header: 'Date de création',
 			dataIndex: 'startDate'
+		},
+		{
+			header: 'Total',
+			dataIndex: 'total'
+		},
+		{
+			header: 'Date de paiement',
+			dataIndex: 'paymentDate'
 		},
 		{
 			header: 'Option',
@@ -71,8 +78,22 @@
 				</td>
 				<td class="px-5 py-3">{row.tenant_fullName}</td>
 				<td class="px-5 py-3">{row.property_address}</td>
-				<td class="px-5 py-3">{row.property_city}</td>
+				<td class="px-5 py-3">
+					<p class="flex flex-col">
+						{row.property_address}
+						<span class="flex space-x-2">
+							{row.property_city}
+							{row.property_postalCode}
+						</span>
+					</p>
+				</td>
 				<td class="px-5 py-3">{dayjs(row.createAt).format('DD/MM/YYYY')}</td>
+				<td class="px-5 py-3">
+					{row.rent + row.condo_fees + row.taxes}
+				</td>
+				<td class="px-5 py-3"
+					>{row.paymentDate ? dayjs(row.paymentDate).format('DD/MM/YYYY') : 'not paid'}</td
+				>
 				<td>
 					<button on:click={() => getPdf(row.id)}>
 						<PrintIcon width="25" class="m-3" />
