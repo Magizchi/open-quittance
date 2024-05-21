de795dcc75ab0b45ec4161c0a01daf8cd49fcfdb
Rajanan <g.rajan1993@gmail.com>
2024-05-21 10:07:42 +0000

    feat(organisms:Form):all used form for create Landlord,rental,tenant,properties

---
+++ src/lib/components/organisms/Forms/createProperty.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Input from '$lib/components/atoms/Input.svelte';
+	import Select from '$lib/components/Select/+page.svelte';
+
+	export let id;
+
+	let selectedLandlord: any;
+</script>
+
+<div>
+	<!-- <Select name="landlord" bind:value={selectedLandlord} options={data} /> -->
+	<Input name={`properties[${id}][name]`} placeholder="Nom du local" value={`mega form ${id}`} />
+	<Input name={`properties[${id}][address]`} placeholder="Adresse" value={`mega form ${id}`} />
+	<Input name={`properties[${id}][city]`} placeholder="Ville" value="mega form" />
+	<Input name={`properties[${id}][postalCode]`} placeholder="Code postal" value="5000" />
+	<Input name={`properties[${id}][rent]`} placeholder="Loyer" value="500" />
+	<Input name={`properties[${id}][condo_fees]`} placeholder="Charge" value="500" />
+	<Input name={`properties[${id}][taxes]`} placeholder="Taxes" value="500" />
+</div>
---
+++ src/lib/components/organisms/Forms/createRental.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Select from '$lib/components/Select/+page.svelte';
+	export let properties;
+	export let tenants;
+
+	let selectProperty: string;
+	let selectTenant: string;
+	let selectedDate: Date;
+</script>
+
+<Select name="property" bind:value={selectProperty} options={properties} />
+<Select name="tenant" bind:value={selectTenant} options={tenants} />
+<input type="date" id="start" name="date" bind:value={selectedDate} min="2000-01-01" />
