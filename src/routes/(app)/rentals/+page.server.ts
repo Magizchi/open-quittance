4d7c746ecb54480f2a5e445c294cef54987aa068
Rajanan <g.rajan1993@gmail.com>
2024-05-21 09:53:42 +0000

    feat(page:rentals):add modal form for create rental

--- src/routes/(app)/rentals/+page.server.ts
+++ src/routes/(app)/rentals/+page.server.ts
@@ -1,5 +1,6 @@
 import db from '$lib/server/database';
 import { landlordsTable, propertiesTable, rentalsTable, tenantsTable } from '$lib/server/schema';
+import dayjs from 'dayjs';
 import { eq } from 'drizzle-orm';
 
 export const load = async () => {
@@ -7,13 +8,12 @@ export const load = async () => {
         await db.select({
             tenant: {
                 id: tenantsTable.id,
-                companyName: tenantsTable.companyName,
-                fullName: tenantsTable.fullName,
+                name: tenantsTable.name,
+
             },
             landlord: {
                 id: landlordsTable.id,
-                fullName: landlordsTable.fullName,
-                companyName: landlordsTable.companyName,
+                name: landlordsTable.name,
             },
             property: {
                 id: propertiesTable.id,
@@ -25,5 +25,60 @@ export const load = async () => {
             .leftJoin(propertiesTable, eq(rentalsTable.property_id, propertiesTable.id))
             .leftJoin(landlordsTable, eq(propertiesTable.landlord_id, landlordsTable.id));
 
-    return { rentals };
+    const propertiesOptions = (await db.select({
+        propertyId: propertiesTable.id,
+        name: propertiesTable.name,
+        city: propertiesTable.city
+    })
+        .from(propertiesTable)
+    ).map(item => {
+        const test = rentals.filter(({ property }) => property?.id === item.propertyId);
+
+        if (test.length > 0) {
+            return ({
+                value: item.propertyId,
+                label: `${item.name}:${item.city}`,
+                description: test[0].tenant?.name
+            });
+        }
+        return ({
+            value: item.propertyId,
+            label: `${item.name}:${item.city}`,
+        });
+    });
+
+    const tenantsOptions = (await db.select({
+        name: tenantsTable.name,
+        id: tenantsTable.id,
+    })
+        .from(tenantsTable) as unknown as { id: string, name: string; }[])
+        .map(item => ({
+            value: item.id,
+            label: item.name
+        }));
+    return { rentals, propertiesOptions, tenantsOptions };
+};
+
+export const actions = {
+    create: async ({ request }) => {
+        const data = await request.formData();
+
+        if (
+            data.get('property') === '' ||
+            data.get('tenant') === '' ||
+            data.get('date') === ''
+        ) {
+            return {
+                success: false,
+                status: 400,
+                message: 'Erreur dans les données'
+            };
+        }
+        await db.insert(rentalsTable).values({
+            tenant_id: +data.get('tenant')!.toString(),
+            property_id: +data.get('property')!.toString(),
+            startedAt: dayjs(data.get('date')!.toString()).toDate()
+        });
+
+    }
 };
--- src/routes/(app)/rentals/+page.svelte
+++ src/routes/(app)/rentals/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import Table from '$lib/components/organisms/table.svelte';
-	export let data;
+	import Modal from '$lib/components/atoms/Modal.svelte';
+	import FormRental from '$lib/components/organisms/Forms/createRental.svelte';
+	import Clickable from '$lib/components/atoms/Clickable.svelte';
 
+	export let data;
+	let showModal = false;
 	const columns = [
 		{
 			header: 'Bailleur',
@@ -23,18 +27,30 @@
 </script>
 
 <section class="flex flex-col p-5">
-	<h1>Location en cours</h1>
+	<div class="flex py-10 m-5">
+		<div class="w-11/12">
+			<h1>Location en cours</h1>
+		</div>
+		<div class="w-1/12">
+			<Clickable on:click={() => (showModal = true)}>Ajouter</Clickable>
+		</div>
+	</div>
 	<Table {columns} rows={data.rentals} let:row>
 		<tr>
 			<td class="px-5 py-3">
 				<p class="flex flex-col">
-					{row.landlord.companyName}
-					<span class="text-xs">{row.landlord.fullName}</span>
+					{row.landlord.name}
 				</p>
 			</td>
-			<td class="px-5 py-3">{row.tenant.fullName}</td>
+			<td class="px-5 py-3">{row.tenant.name}</td>
 			<td class="px-5 py-3">{row.property.name}</td>
 			<td class="px-5 py-3">{row.property.city}</td>
 		</tr>
 	</Table>
+	<Modal bind:showModal>
+		<form method="POST" class="space-y-10" action="?/create">
+			<FormRental properties={data.propertiesOptions} tenants={data.tenantsOptions} />
+			<button type="submit">submit</button>
+		</form>
+	</Modal>
 </section>
