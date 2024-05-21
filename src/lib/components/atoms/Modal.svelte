97895c706d1aa05dc7d7bc84686a16e52c0e369b
Rajanan <g.rajan1993@gmail.com>
2024-05-21 10:14:30 +0000

    feat(atom:modal):create modal component

---
+++ src/lib/components/atoms/Modal.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let showModal: Boolean;
+	let dialog: HTMLDialogElement;
+
+	$: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+>
+	<div>
+		<slot name="header" />
+		<hr />
+		<slot />
+		<hr />
+		<button on:click={() => dialog.close()}>close modal</button>
+	</div>
+</dialog>
