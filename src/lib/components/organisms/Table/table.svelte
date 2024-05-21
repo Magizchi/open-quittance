04bbba02e66fd77eacd6583c41b0b4069c9fddf1
Rajanan <g.rajan1993@gmail.com>
2024-05-21 10:19:40 +0000

    feat(organisms:Table):create table and td

---
+++ src/lib/components/organisms/Table/table.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface ColumnsProps {
+		header: string;
+		dataIndex: string;
+	}
+
+	export let columns: ColumnsProps[];
+	export let rows: any[];
+</script>
+
+<div class="m-5 overflow-hidden border border-gray-200 rounded-lg shadow-md">
+	<table class="w-full text-sm text-left text-gray-500 bg-white border-collapse">
+		<thead class="w-full bg-gray-100">
+			<tr>
+				{#each columns as column}
+					<th class="">
+						{column.header}
+					</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each rows as row, id}
+				<slot {row} {id} />
+			{/each}
+		</tbody>
+	</table>
+</div>
