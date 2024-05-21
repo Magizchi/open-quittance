b7a8001e3fa234aa4977dbe3d1227e1f7e9af5db
Rajanan <g.rajan1993@gmail.com>
2024-05-21 10:06:28 +0000

    delete(page:landlord):delete page

--- src/routes/(app)/landlords/create/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import FormForUser from '$lib/components/organisms/Forms/createUser.svelte';
-	export let form;
-	console.log('form', form);
-</script>
-
-<div class="max-w-2xl p-16 mx-auto">
-	<FormForUser />
-</div>
