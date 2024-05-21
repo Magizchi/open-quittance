34ff560e6099efa9aa9fefd027178bd9bdc1068b
Rajanan <g.rajan1993@gmail.com>
2024-05-21 10:17:47 +0000

    feat(atom:select):create

---
+++ src/lib/components/atoms/Select.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let options: any;
+	export let value: any;
+</script>
+
+<select bind:value name="" id="" {...$$restProps}>
+	<option value="" selected> choisir... </option>
+	{#each options as option}
+		<option value={option.value} disabled={option.description ? true : false}>
+			<div class="flex flex-col">
+				<span class="bg-red-400">{option.label}</span>
+				{#if option.description}
+					<span>{option.description}</span>
+				{/if}
+			</div>
+		</option>
+	{/each}
+</select>
