3c9f5179e2f7df4c4411b38b3ad4d481ba4b645a
Rajanan <g.rajan1993@gmail.com>
2024-05-22 17:17:07 +0000

    style(layout:sideMenu):add aria-current&path

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,36 +1,63 @@
 <script lang="ts">
 	import Bread from '$lib/components/bread/+page.svelte';
+	import { page } from '$app/stores';
+
 	// export let data;
 </script>
 
-<div class="flex">
-	<aside class="min-h-screen p-4 text-white bg-gray-800 w-60">
+<div class="flex bg-slate-300">
+	<aside class="min-h-screen p-4 text-slate-600 w-60">
 		<div class="w-full px-2 py-4 mb-5">
-			<h1>OpenQuittance</h1>
+			<h1><a href="/">OpenQuittance</a></h1>
 		</div>
 		<nav>
 			<ul class="space-y-3">
 				<li class="w-full">
-					<a href="/" class="flex p-2 hover:bg-gray-700">Accueil</a>
+					<a
+						href="/"
+						aria-current={$page.url.pathname === '/'}
+						class="flex p-2 hover:bg-slate-200 rounded-lg hover:text-slate-700 aria-[current=true]:bg-slate-200"
+						>Accueil</a
+					>
 				</li>
 				<li class="w-full space-y-2">
-					<a href="/landlords" class="flex p-2 hover:bg-gray-700">Propriétaires</a>
+					<a
+						href="/landlords"
+						aria-current={$page.url.pathname === '/landlords'}
+						class="flex p-2 hover:bg-slate-200 rounded-lg hover:text-slate-700 aria-[current=true]:bg-slate-200"
+						>Propriétaires</a
+					>
 				</li>
 				<li>
-					<a href="/tenants" class="flex p-2 hover:bg-gray-700">Locataires</a>
+					<a
+						aria-current={$page.url.pathname === '/tenants'}
+						href="/tenants"
+						class="flex p-2 hover:bg-slate-200 rounded-lg hover:text-slate-700 aria-[current=true]:bg-slate-200"
+						>Locataires</a
+					>
 				</li>
 
 				<li>
-					<a href="/rentals" class="flex p-2 hover:bg-gray-700">Locations</a>
+					<a
+						href="/rentals"
+						aria-current={$page.url.pathname === '/rentals'}
+						class="flex p-2 hover:bg-slate-200 rounded-lg hover:text-slate-700 aria-[current=true]:bg-slate-200"
+						>Locations</a
+					>
 				</li>
 				<li>
-					<a href="/receipts" class="flex p-2 hover:bg-gray-700">Quittances</a>
+					<a
+						href="/receipts"
+						aria-current={$page.url.pathname === '/receipts'}
+						class="flex p-2 hover:bg-slate-200 rounded-lg hover:text-slate-700 aria-[current=true]:bg-slate-200"
+						>Quittances</a
+					>
 				</li>
 			</ul>
 		</nav>
 	</aside>
-	<div class="flex flex-col w-full">
-		<header class="w-full h-16 p-5 text-gray-700 bg-white shadow-sm body-font">
+	<div class="flex flex-col w-full m-2 bg-white rounded-xl">
+		<header class="w-full h-16 p-5 text-gray-700 bg-white body-font rounded-t-xl">
 			<Bread />
 		</header>
 		<main>
