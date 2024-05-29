d0807856b74a7b1c494a425f8704fa78950f983e
Rajanan <g.rajan1993@gmail.com>
2024-05-29 11:42:59 +0000

    feat(atom:icon):add Ben&Building icon

--- src/lib/components/atoms/Icons/icon.ts
+++ src/lib/components/atoms/Icons/icon.ts
@@ -2,3 +2,5 @@ export { default as UserIcon } from "./userIcon.svelte";
 export { default as KeyIcon } from "./keyIcon.svelte";
 export { default as PrintIcon } from "./PrintIcon.svelte";
 export { default as PenIcon } from "./PenIcon.svelte";
+export { default as BenIcon } from "./BenIcon.svelte";
+export { default as BuildingIcon } from './BuildingIcon.svelte';
