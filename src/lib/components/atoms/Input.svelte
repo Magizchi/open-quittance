dcda6a63ff85c035be8e8a05b3f5b5d4d67570b9
Rajanan <g.rajan1993@gmail.com>
2024-05-29 11:43:37 +0000

    style(atome:input):add label for input

--- src/lib/components/atoms/Input.svelte
+++ src/lib/components/atoms/Input.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
 	export let name: string;
 	export let className: string | null = null;
+	export let label: string | null = null;
 </script>
 
-<slot name="label" />
-<div class="flex">
-	{#if $$slots.CustomIcon}
-		<span
-			class={'flex items-center justify-center h-10 text-2xl text-gray-400 border border-r-0 rounded-l-lg min-w-10 z-highest' +
-				className}
-		>
-			<slot name="CustomIcon" />
-		</span>
+<div class="flex flex-col">
+	{#if label}
+		<label for={name}>{label}</label>
 	{/if}
-	<input
-		type="text"
-		id={name}
-		class={$$slots.CustomIcon
-			? 'w-full pl-1 border border-gray-200 rounded-r-lg outline-none focus:ring-1 ring-blue-400'
-			: 'border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}
-		{name}
-		{...$$restProps}
-	/>
+	<div class="flex">
+		{#if $$slots.CustomIcon}
+			<span
+				class={'flex items-center justify-center h-10 text-2xl text-gray-400 border border-r-0 rounded-l-lg min-w-10 z-highest' +
+					className}
+			>
+				<slot name="CustomIcon" />
+			</span>
+		{/if}
+		<input
+			type="text"
+			id={name}
+			class={$$slots.CustomIcon
+				? 'w-full pl-1 border border-gray-200 rounded-r-lg outline-none focus:ring-1 ring-blue-400'
+				: 'border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}
+			{name}
+			{...$$restProps}
+		/>
+	</div>
 </div>
