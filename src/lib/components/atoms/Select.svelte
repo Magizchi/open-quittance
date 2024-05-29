36ed38b8c3124fb7d304ca1cb240e3d5b5ba309a
Rajanan <g.rajan1993@gmail.com>
2024-05-29 11:44:20 +0000

    style(atom:select):add label

--- src/lib/components/atoms/Select.svelte
+++ src/lib/components/atoms/Select.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	export let options: any;
 	export let value: any;
+	export let label: string;
+	export let name: string;
 </script>
 
-<select bind:value name="" id="" {...$$restProps}>
-	<option value="" selected> choisir... </option>
-	{#each options as option}
-		<option value={option.value} disabled={option.description ? true : false}>
-			<div class="flex flex-col">
-				<span class="bg-red-400">{option.label}</span>
-				{#if option.description}
-					<span>{option.description}</span>
-				{/if}
-			</div>
-		</option>
-	{/each}
-</select>
+<div class="flex flex-col space-y-2">
+	<label for={'select' + name}>{label}</label>
+	<select bind:value {...$$restProps}>
+		<option value="" selected> choisir... </option>
+		{#each options as option}
+			<option value={option.value} disabled={option.description ? true : false}>
+				<div class="flex flex-col">
+					<span class="bg-red-400">{option.label}</span>
+					{#if option.description}
+						<span>{option.description}</span>
+					{/if}
+				</div>
+			</option>
+		{/each}
+	</select>
+</div>
