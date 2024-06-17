a8af724144b44ea1dff6cd7ba411fb9829eb746b
Rajanan <g.rajan1993@gmail.com>
2024-06-17 09:35:51 +0000

    feat(atom:select):add restProps to component

--- src/lib/components/atoms/Select.svelte
+++ src/lib/components/atoms/Select.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex flex-col space-y-2">
 	<label for={'select' + name}>{label}</label>
-	<select bind:value {...$$restProps}>
+	<select {name} bind:value {...$$restProps}>
 		<option value="" selected> choisir... </option>
 		{#each options as option}
 			<option value={option.value} disabled={option.description ? true : false}>
