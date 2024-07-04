06d332a963331e7abf1493264f5955b027af1d44
Rajanan <g.rajan1993@gmail.com>
2024-07-04 12:31:29 +0000

    feat(atom:notifcation):create

---
+++ src/lib/components/atoms/notification/notification.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let showModal: Boolean;
+	let dialog: HTMLDialogElement;
+
+	$: if (dialog && showModal) dialog.showModal();
+	$: if (dialog && !showModal) dialog.close();
+
+	export let success: boolean = false;
+	export let message: string = '';
+	let className: string;
+
+	if (success) {
+		className =
+			'absolute top-0 right-0 p-5 shadow-md rounded-2xl scroll-m-10 min-w-[476px] bg-green-500';
+	} else {
+		className =
+			'absolute top-0 right-0 p-5 shadow-md rounded-2xl scroll-m-10 min-w-[476px] bg-red-500';
+	}
+
+	$: if (showModal) {
+		setTimeout(() => {
+			console.log('Dans setTimeout');
+			showModal = false;
+		}, 5000);
+	}
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+	class={className}
+	{...$$restProps}
+>
+	<div class="relative">
+		{message}
+		<div class="absolute top-0 right-0">
+			<button on:click={() => dialog.close()}>X</button>
+		</div>
+	</div>
+</dialog>
