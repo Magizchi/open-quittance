def0d50a02ff4f8e802e76f6df217d83a9e2248b
Rajanan <g.rajan1993@gmail.com>
2024-07-13 17:19:16 +0000

    refacto(page:landord)

---
+++ src/routes/(app)/landlords/columns.ts
@@ -0,0 +1,30 @@
+export const columns = [
+    {
+        header: 'Propriétaire',
+        dataIndex: 'name'
+    },
+    {
+        header: 'Siret',
+        dataIndex: 'siret'
+    },
+    {
+        header: 'Adresse',
+        dataIndex: 'address'
+    },
+    {
+        header: 'Ville',
+        dataIndex: 'city'
+    },
+    {
+        header: 'Code Postal',
+        dataIndex: 'postalCode'
+    },
+    {
+        header: 'Nombre de propriété',
+        dataIndex: 'properties'
+    },
+    {
+        header: 'Options',
+        dataIndex: 'options'
+    }
+];
