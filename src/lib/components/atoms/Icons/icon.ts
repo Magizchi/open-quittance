add56539d080d94f87ece64dce9cec73bead42cd
Rajanan <g.rajan1993@gmail.com>
2024-07-14 19:43:46 +0000

    change icons

--- src/lib/components/atoms/Icons/icon.ts
+++ src/lib/components/atoms/Icons/icon.ts
@@ -4,3 +4,4 @@ export { default as PrintIcon } from "./PrintIcon.svelte";
 export { default as PenIcon } from "./PenIcon.svelte";
 export { default as BenIcon } from "./BenIcon.svelte";
 export { default as BuildingIcon } from './BuildingIcon.svelte';
+export { default as PdfIcon } from "./PdfIcon.svelte";
