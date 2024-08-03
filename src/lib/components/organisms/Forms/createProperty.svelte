0f685d81f46b5ea32b70af3b0c3d0beca2a8d85f
Rajanan <g.rajan1993@gmail.com>
2024-08-03 13:57:04 +0000

    feat:(form:createProperty): add label

--- src/lib/components/organisms/Forms/createProperty.svelte
+++ src/lib/components/organisms/Forms/createProperty.svelte
@@ -12,32 +12,59 @@
 	};
 </script>
 
-<div class="space-y-2">
-	<Input
-		name={`properties[${id}][name]`}
-		bind:value={defaultValues['name']}
-		placeholder="Nom du local"
-	/>
-	<Input
-		name={`properties[${id}][address]`}
-		bind:value={defaultValues['address']}
-		placeholder="Adresse"
-	/>
-	<Input name={`properties[${id}][city]`} bind:value={defaultValues['city']} placeholder="Ville" />
-	<Input
-		name={`properties[${id}][postalCode]`}
-		bind:value={defaultValues['postalCode']}
-		placeholder="Code postal"
-	/>
-	<Input name={`properties[${id}][rent]`} bind:value={defaultValues['rent']} placeholder="Loyer" />
-	<Input
-		name={`properties[${id}][condo_fees]`}
-		bind:value={defaultValues['condo_fees']}
-		placeholder="Charge"
-	/>
-	<Input
-		name={`properties[${id}][taxes]`}
-		bind:value={defaultValues['taxes']}
-		placeholder="Taxes"
-	/>
+<div class="grid grid-cols-2 gap-2">
+	<div class="col-span-2">
+		<Input
+			label="Local"
+			name={`properties[${id}][name]`}
+			bind:value={defaultValues['name']}
+			placeholder="local de bobigny"
+		/>
+	</div>
+	<div class="col-span-2">
+		<Input
+			label="Adresse"
+			name={`properties[${id}][address]`}
+			bind:value={defaultValues['address']}
+			placeholder="1, Avenue de paris"
+		/>
+	</div>
+	<div class="col-span-">
+		<Input
+			name={`properties[${id}][city]`}
+			bind:value={defaultValues['city']}
+			placeholder="Ville"
+		/>
+	</div>
+	<div class="col-span-1">
+		<Input
+			name={`properties[${id}][postalCode]`}
+			bind:value={defaultValues['postalCode']}
+			placeholder="Code postal"
+		/>
+	</div>
+	<div class="col-span-2">
+		<Input
+			label="Loyer"
+			name={`properties[${id}][rent]`}
+			bind:value={defaultValues['rent']}
+			placeholder="1100"
+		/>
+	</div>
+	<div class="col-span-2">
+		<Input
+			label="Charges"
+			name={`properties[${id}][condo_fees]`}
+			bind:value={defaultValues['condo_fees']}
+			placeholder="510"
+		/>
+	</div>
+	<div class="col-span-2">
+		<Input
+			label="Taxes"
+			name={`properties[${id}][taxes]`}
+			bind:value={defaultValues['taxes']}
+			placeholder="90"
+		/>
+	</div>
 </div>
