1b4a71fdf240707f1fb02e103ea2d6a7642d1389
Rajanan <g.rajan1993@gmail.com>
2024-08-05 14:20:36 +0000

    type:(model:receiptType)

--- src/lib/models/receipts.model.ts
+++ src/lib/models/receipts.model.ts
@@ -5,30 +5,30 @@
 interface receiptType {
     id: number;
     number: string | number,
-    landlord_id: string;
+    landlord_id: string | number;
     landlord_fullName: string;
     landlord_address: string;
     landlord_city: string;
     landlord_postalCode: string;
 
-    tenant_id: string;
+    tenant_id: string | number;
     tenant_address: string;
     tenant_city: string;
     tenant_fullName: string;
     tenant_postalCode: string;
 
-    rent: string;
-    condo_fees: string;
-    taxes: string;
+    rent: string | number;
+    condo_fees: string | number;
+    taxes: string | number;
 
     property_address: string;
     property_city: string;
     property_postalCode: string;
 
     paymentDate: null | string | Date,
-    startDate: string | Date;
-    endDate: string | Date;
-    createAt: string | Date;
+    startDate: null | string | Date;
+    endDate: null | string | Date;
+    createAt: null | string | Date;
 }
 
 export default interface ReceiptsModel extends receiptType { }
