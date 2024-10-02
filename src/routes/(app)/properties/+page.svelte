9dbb1a457ba6b4df4a187e6513953169bc7f7457
Rajanan <g.rajan1993@gmail.com>
2024-10-02 12:24:53 +0000

    page:add properties

---
+++ src/routes/(app)/properties/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import FormProperty from '$lib/components/organisms/Forms/createProperty.svelte';
+	import Clickable from '$lib/components/atoms/Clickable.svelte';
+	import { Table, Tr, Td } from '$lib/components/organisms/Table';
+	import Modal from '$lib/components/atoms/Modal.svelte';
+	import BuildingIcon from '$lib/components/atoms/Icons/BuildingIcon.svelte';
+	import FromCreateProperties from '$lib/components/organisms/Forms/formCreateProperties.svelte';
+	import type { PropertyModel } from '$lib/models';
+	import { createNotification } from '$lib/stores/notification/store.js';
+	import { columns } from './columns.js';
+	import { goto } from '$app/navigation';
+
+	export let data;
+</script>
+
+<section class="px-10 space-y-3">
+	<div class="flex justify-between">
+		<h1 class="text-2xl font-bold">Liste des Propriété</h1>
+		<!-- <Clickable href="/landlords/create" on:click={() => (showLandlordForm = true)}
+			>Créer Propriétaire</Clickable
+		> -->
+	</div>
+	<Table {columns} rows={data.properties} let:row>
+		<Tr>
+			<Td>
+				<a href={'/landlords/' + row.id} class="underline">
+					{row.name}
+				</a>
+			</Td>
+			<Td>
+				{row.address}
+			</Td>
+			<Td>
+				{row.city}
+			</Td>
+			<Td>
+				{row.postalCode}
+			</Td>
+			<Td>
+				{row.rent}
+			</Td>
+			<Td>
+				{row.condo_fees}
+			</Td>
+			<Td>
+				{row.taxes}
+			</Td>
+		</Tr>
+	</Table>
+</section>
---
+++ src/routes/(app)/properties/columns.ts
@@ -0,0 +1,30 @@
+export const columns = [
+    {
+        header: 'Local',
+        dataIndex: 'name'
+    },
+    {
+        header: 'Adresse',
+        dataIndex: 'address'
+    },
+    {
+        header: 'Ville',
+        dataIndex: 'city'
+    },
+    {
+        header: 'Code Postal',
+        dataIndex: 'postalCode'
+    },
+    {
+        header: 'Loyer',
+        dataIndex: 'rent'
+    },
+    {
+        header: 'Charges',
+        dataIndex: 'condo_fees'
+    },
+    {
+        header: 'Taxe',
+        dataIndex: 'taxes'
+    },
+];
