2ef6d823c2a4fcb1ce92e24e5ecc9b4d8d3e953f
Rajanan <g.rajan1993@gmail.com>
2024-10-04 12:17:36 +0000

    style:inputs:add default color and fonts

--- src/lib/components/atoms/Input.svelte
+++ src/lib/components/atoms/Input.svelte
@@ -6,12 +6,12 @@
 
 <div class="flex flex-col">
 	{#if label}
-		<label for={name}>{label}</label>
+		<label class="text-slate-700 font-hind" for={name}>{label}</label>
 	{/if}
 	<div class="flex">
 		{#if $$slots.CustomIcon}
 			<span
-				class={'flex items-center justify-center h-10 text-2xl text-gray-400 border border-r-0 rounded-l-lg min-w-10 z-highest' +
+				class={'flex items-center justify-center h-10 text-2xl text-slate-700 border border-r-0 rounded-l-lg min-w-10 z-highest' +
 					className}
 			>
 				<slot name="CustomIcon" />
@@ -22,7 +22,7 @@
 			id={name}
 			class={$$slots.CustomIcon
 				? 'w-full pl-1 border border-gray-200 rounded-r-lg outline-none focus:ring-1 ring-blue-400'
-				: 'border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'}
+				: 'placeholder:font-hind font-hind border text-sm rounded-lg focus:placeholder:text-indigo-500 focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5'}
 			{name}
 			{...$$restProps}
 		/>
