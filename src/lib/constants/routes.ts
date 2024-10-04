13b2bde3db9c5383c8de94e71aa98ca0467a5bb6
Rajanan <g.rajan1993@gmail.com>
2024-10-04 18:19:57 +0000

    docs:constant:added

---
+++ src/lib/constants/routes.ts
@@ -0,0 +1,8 @@
+export enum Routes {
+    login = '/login',
+    landing = '/',
+    createLandlords = '/landlords/create',
+    properties = '/properties',
+    tenants = '/tenants',
+    rentals = '/rentals',
+};
