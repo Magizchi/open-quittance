6095dd9e90c5b13470975acdd3b0ad27ded70263
Rajanan <g.rajan1993@gmail.com>
2024-10-06 10:43:01 +0000

    feat(organism:form)form create landlord

---
+++ src/lib/components/organisms/Forms/createLandlord.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Clickable from '$lib/components/atoms/Clickable.svelte';
+	import Input from '$lib/components/atoms/Input.svelte';
+</script>
+
+<div class="p-3 space-y-8">
+	<div class="grid gap-3 lg:grid-cols-2">
+		<div class="col-span-2">
+			<Input
+				label="Nom du Bailleur*"
+				placeholder="Nom du bailleur"
+				name="landlordName"
+				required="required"
+			/>
+			<p class="text-sm italic text-slate-600 font-hind">
+				Nom et prénom si vous ête un particulier sinon le nom de l'entreprise
+			</p>
+		</div>
+		<div class="col-span-2">
+			<Input label="Adresse*" placeholder="Address" name="address" required="required" />
+		</div>
+		<Input placeholder="Code postal" name="postalCode" required="required" />
+		<Input placeholder="Ville" name="city" required="required" />
+	</div>
+	<div class="flex justify-end">
+		<Clickable success type="submit">Enregistrer</Clickable>
+	</div>
+</div>
