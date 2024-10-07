0c9bd7122f58b6d96de37561fae4bab347354947
Rajanan <g.rajan1993@gmail.com>
2024-10-07 08:11:18 +0000

    refacto(page:properties):refacto code

--- src/routes/(app)/properties/+page.server.ts
+++ src/routes/(app)/properties/+page.server.ts
@@ -1,79 +1,10 @@
 import db from '$lib/server/database';
-import { landlordsTable, propertiesTable } from '$lib/server/schema.js';
-import FormatFormData from '$lib/utils/FormatFormData.js';
-import { getSession } from '$lib/utils/Session.js';
-import { redirect } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
+import { propertiesTable } from '$lib/server/schema.js';
 
-export const load = async ({ parent, cookies }) => {
 
+export const load = async ({ parent }) => {
     await parent();
-    const sessionInfo = getSession(cookies);
-    if (!sessionInfo) {
-        return { properties: [] };
-    }
-    const properties = await db.select().from(propertiesTable).where(eq(propertiesTable.landlord_id, sessionInfo.landlordId));
 
+    const properties = await db.select().from(propertiesTable);
     return { properties };
-};
-
-export const actions = {
-    create: async ({ request }) => {
-        const data = await request.formData();
-
-        const { landlord, error } = FormatFormData(data);
-
-        if (error.status) return {
-            success: false,
-            status: 400,
-            message: "Erreur:" + error.message
-        };
-
-        try {
-            await db.transaction(async (tx) => {
-                //Save the landlords first to give the ID 
-                await tx.insert(landlordsTable).values(landlord);
-                //Add the new landlord Id to the properties before save
-                // properties.forEach(item => item['landlord_id'] = savedLandlord.insertId);
-                // const [saveProperties] = await tx.insert(propertiesTable).values(properties);
-
-            });
-            return {
-                success: true,
-                status: 302,
-                message: 'Created'
-            };
-        } catch (err) {
-            return {
-                success: false,
-                status: 400,
-                message: "Erreur:" + err
-            };
-        }
-
-        redirect(302, '/landlords/9');
-    },
-
-    add: async ({ request }) => {
-        const data = await request.formData();
-        const { properties, error } = FormatFormData(data);
-
-        if (error.status) return error;
-
-        try {
-            await db.insert(propertiesTable).values(properties);
-            return {
-                success: true,
-                status: 201,
-                message: "Saved"
-            };
-        } catch (err) {
-            return {
-                success: false,
-                status: 400,
-                message: "Erreur:" + err
-            };
-        }
-
-    }
 };
--- src/routes/(app)/properties/+page.svelte
+++ src/routes/(app)/properties/+page.svelte
@@ -1,29 +1,21 @@
 <script lang="ts">
-	import FormProperty from '$lib/components/organisms/Forms/createProperty.svelte';
 	import Clickable from '$lib/components/atoms/Clickable.svelte';
 	import { Table, Tr, Td } from '$lib/components/organisms/Table';
-	import Modal from '$lib/components/atoms/Modal.svelte';
-	import BuildingIcon from '$lib/components/atoms/Icons/BuildingIcon.svelte';
-	import FromCreateProperties from '$lib/components/organisms/Forms/formCreateProperties.svelte';
-	import type { PropertyModel } from '$lib/models';
-	import { createNotification } from '$lib/stores/notification/store.js';
 	import { columns } from './columns.js';
-	import { goto } from '$app/navigation';
+	import { Routes } from '$lib/constants/routes.js';
 
 	export let data;
 </script>
 
 <section class="px-10 space-y-3">
 	<div class="flex justify-between">
-		<h1 class="text-2xl font-bold">Liste des Propriété</h1>
-		<!-- <Clickable href="/landlords/create" on:click={() => (showLandlordForm = true)}
-			>Créer Propriétaire</Clickable
-		> -->
+		<h1 class="text-2xl font-bold font-hind text-slate-700">Liste des Propriété</h1>
+		<Clickable primary href={Routes.createProperty}>Créer Propriétaire</Clickable>
 	</div>
 	<Table {columns} rows={data.properties} let:row>
 		<Tr>
 			<Td>
-				<a href={'/landlords/' + row.id} class="underline">
+				<a href={Routes.properties + '/' + row.id} class="underline">
 					{row.name}
 				</a>
 			</Td>
