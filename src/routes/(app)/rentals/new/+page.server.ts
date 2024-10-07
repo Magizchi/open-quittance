85ac3814d4ee9fe78b6a66a8d940d4b8d13f9f4f
Rajanan <g.rajan1993@gmail.com>
2024-10-07 12:00:55 +0000

    feat(page:rental):form create new rentals

---
+++ src/routes/(app)/rentals/new/+page.server.ts
@@ -0,0 +1,34 @@
+import FormDataToJson from "$lib/utils/FormDataToJson.js";
+import { redirect } from "@sveltejs/kit";
+import db from "$lib/server/database";
+import { propertiesTable, rentalsTable, tenantsTable } from "$lib/server/schema.js";
+import dayjs from "dayjs";
+
+export const load = async ({ parent }) => {
+    await parent();
+    const propertiesOptions = (await db.select().from(propertiesTable)).map(element => ({ label: element.name, value: element.id }));
+    const tenantsOptions = (await db.select().from(tenantsTable)).map(element => ({ label: element.name, value: element.id }));
+
+    return { propertiesOptions, tenantsOptions };
+};
+
+export const actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
+        const { property, tenant, date } = FormDataToJson(data);
+
+        try {
+            await db.insert(rentalsTable).values({
+                property_id: +property, tenant_id: +tenant, startedAt: dayjs(date.toString()).toDate()
+            });
+        } catch (err) {
+            return {
+                message: 'err' + err,
+                success: false,
+                status: 400
+            };
+        }
+
+        throw redirect(303, "/");
+    }
+};
---
+++ src/routes/(app)/rentals/new/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Clickable from '$lib/components/atoms/Clickable.svelte';
+	import { Routes } from '$lib/constants/routes';
+	import FormRental from '$lib/components/organisms/Forms/createRental.svelte';
+
+	export let data;
+</script>
+
+<section class="flex items-center justify-center w-full mt-10">
+	<div class="flex flex-col max-w-xl bg-white shadow-lg rounded-xl">
+		<div class="p-5 space-y-5 bg-indigo-500 rounded-t-xl">
+			<h2 class="text-3xl text-white font-hind">Création d'une location</h2>
+			<p class="text-base text-white font-hind">
+				Les informations ci-dessous seront nécessaires pour la génération des quittances.
+			</p>
+		</div>
+		<div class="p-5">
+			<form id="landlord" method="POST" class="space-y-5">
+				<FormRental properties={data.propertiesOptions} tenants={data.tenantsOptions} />
+				<div class="flex space-x-5">
+					<Clickable secondary likeButton href={Routes.rentals}>Annuler</Clickable>
+					<Clickable primary type="submit">Ajouter</Clickable>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
