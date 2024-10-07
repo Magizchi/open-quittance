80dc221575ced9be9f4c6dd2e045716e59b00ff4
Rajanan <g.rajan1993@gmail.com>
2024-10-07 12:00:00 +0000

    refacto(page:rentals)

--- src/routes/(app)/rentals/+page.svelte
+++ src/routes/(app)/rentals/+page.svelte
@@ -3,55 +3,22 @@
 	import FormRental from '$lib/components/organisms/Forms/createRental.svelte';
 	import Clickable from '$lib/components/atoms/Clickable.svelte';
 	import { Table, Tr, Td } from '$lib/components/organisms/Table';
-	import dayjs from 'dayjs';
 	import Input from '$lib/components/atoms/Input.svelte';
 	import type RentalModel from '$lib/models/rental.model.js';
-
+	import { columns } from './columns';
+	import { Routes } from '$lib/constants/routes.js';
+	import { formatDate } from '$lib/utils/date/index.js';
 	export let data;
 	let showModal = false;
 	let showModalEndRentals = false;
-	const columns = [
-		{
-			header: 'Bailleur',
-			dataIndex: 'landlord'
-		},
-		{
-			header: 'Locataire',
-			dataIndex: 'tenant'
-		},
-		{
-			header: 'Propriété',
-			dataIndex: 'property'
-		},
-		{
-			header: 'Ville',
-			dataIndex: 'city'
-		},
-		{
-			header: 'Début de location',
-			dataIndex: 'startDate'
-		},
-		{
-			header: 'Fin de location',
-			dataIndex: 'endDate'
-		},
-		{
-			header: 'Options',
-			dataIndex: 'options'
-		}
-	];
 
 	let rental = {} as RentalModel;
 </script>
 
 <section class="px-10 space-y-3">
-	<div class="flex">
-		<div class="w-11/12">
-			<h1 class="text-2xl font-bold">Liste des Locations en cours</h1>
-		</div>
-		<div class="w-1/12">
-			<Clickable on:click={() => (showModal = true)}>Ajouter</Clickable>
-		</div>
+	<div class="flex justify-between">
+		<h1 class="text-2xl font-bold font-hind text-slate-700">Liste des Locations en cours</h1>
+		<Clickable primary href={Routes.createRental}>Ajouter</Clickable>
 	</div>
 	<Table {columns} rows={data.rentals} let:row>
 		<Tr>
@@ -63,8 +30,8 @@
 			<Td>{row.tenant.name}</Td>
 			<Td>{row.property.name}</Td>
 			<Td>{row.property.city}</Td>
-			<Td>{dayjs(row.startDate).format('DD/MM/YYYY')}</Td>
-			<Td>{row.endDate ? dayjs(row.endDate).format('DD/MM/YYYY') : '/'}</Td>
+			<Td>{formatDate(row.startDate)}</Td>
+			<Td>{row.endDate ? formatDate(row.endDate) : '/'}</Td>
 			<Td>
 				<div>
 					<Clickable
---
+++ src/routes/(app)/rentals/columns.ts
@@ -0,0 +1,30 @@
+export const columns = [
+    {
+        header: 'Bailleur',
+        dataIndex: 'landlord'
+    },
+    {
+        header: 'Locataire',
+        dataIndex: 'tenant'
+    },
+    {
+        header: 'Propriété',
+        dataIndex: 'property'
+    },
+    {
+        header: 'Ville',
+        dataIndex: 'city'
+    },
+    {
+        header: 'Début de location',
+        dataIndex: 'startDate'
+    },
+    {
+        header: 'Fin de location',
+        dataIndex: 'endDate'
+    },
+    {
+        header: 'Options',
+        dataIndex: 'options'
+    }
+];
