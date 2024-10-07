a696b185f5ea194bee3ff24eaf1dc418e2eb1c21
Rajanan <g.rajan1993@gmail.com>
2024-10-07 10:58:16 +0000

    feat(page:tenant:id):form update tenant

---
+++ src/routes/(app)/tenants/[...id]/+page.server.ts
@@ -0,0 +1,43 @@
+import FormDataToJson from "$lib/utils/FormDataToJson.js";
+import { redirect } from "@sveltejs/kit";
+import db from "$lib/server/database";
+import { tenantsTable } from "$lib/server/schema.js";
+import { eq } from "drizzle-orm";
+import { Routes } from "$lib/constants/routes";
+import { createNotification } from "$lib/stores/notification/store.js";
+
+export const load = async ({ parent, params }) => {
+    await parent();
+
+    const [tenant] = await db.select().from(tenantsTable).where(eq(tenantsTable.id, +params.id));
+
+    if (!tenant) {
+        throw redirect(303, Routes.tenants);
+    }
+
+    return { tenant };
+};
+
+export const actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
+        const { id, name, address, city, postalCode } = FormDataToJson(data);
+
+        try {
+            await db.update(tenantsTable).set({
+                name,
+                address,
+                city,
+                postalCode,
+            }).where(eq(tenantsTable.id, +id));
+        } catch (err) {
+            return {
+                message: 'err' + err,
+                success: false,
+                status: 400
+            };
+        }
+        createNotification({ message: `Locataire ${name} a bien été modifié`, success: true });
+        throw redirect(303, Routes.tenants);
+    }
+};
---
+++ src/routes/(app)/tenants/[...id]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Clickable from '$lib/components/atoms/Clickable.svelte';
+	import FormForUser from '$lib/components/organisms/Forms/createUser.svelte';
+	import { Routes } from '$lib/constants/routes.js';
+	export let data;
+</script>
+
+<section class="flex items-center justify-center w-full mt-10">
+	<div class="flex flex-col max-w-xl bg-white shadow-lg rounded-xl">
+		<div class="p-5 space-y-5 bg-indigo-500 rounded-t-xl">
+			<h2 class="text-3xl text-white font-hind">Modifier le locataire: "{data.tenant.name}"</h2>
+			<p class="text-base text-white font-hind">
+				Les informations ci-dessous seront nécessaires pour la génération des quittances.
+			</p>
+		</div>
+		<div class="p-5">
+			<form id="landlord" method="POST" class="space-y-5">
+				<FormForUser defaultValues={data.tenant} />
+				<input name="id" value={data.tenant.id} hidden />
+				<div class="flex space-x-5">
+					<Clickable secondary likeButton href={Routes.tenants}>Annuler</Clickable>
+					<Clickable primary type="submit">Modifier</Clickable>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
