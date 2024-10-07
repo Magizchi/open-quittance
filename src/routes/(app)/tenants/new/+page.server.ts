d6cdce0698831265f923e772e040463b7f5b1483
Rajanan <g.rajan1993@gmail.com>
2024-10-07 10:04:02 +0000

    feat(page:tenant/create):form create tenant

---
+++ src/routes/(app)/tenants/new/+page.server.ts
@@ -0,0 +1,49 @@
+import FormDataToJson from "$lib/utils/FormDataToJson.js";
+import { redirect } from "@sveltejs/kit";
+import db from "$lib/server/database";
+import { tenantsTable } from "$lib/server/schema.js";
+import { createNotification } from "$lib/stores/notification/store.js";
+import { Routes } from "$lib/constants/routes.js";
+
+export const load = async ({ parent }) => {
+    await parent();
+};
+
+export const actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
+        const { name, address, city, postalCode } = FormDataToJson(data);
+        if (
+            name === '' ||
+            address === '' ||
+            city === '' ||
+            postalCode === ''
+        ) {
+            createNotification({ message: "Erreur dans les données", status: 400, success: false });
+            return {
+                success: false,
+                status: 400,
+                message: "Erreur dans les données"
+            };
+        }
+
+        try {
+            await db.insert(tenantsTable).values({
+                name,
+                address,
+                city,
+                postalCode,
+            });
+        } catch (err) {
+            createNotification({ message: "Erreur dans les données", status: 400, success: false });
+            return {
+                success: false,
+                status: 400,
+                message: "Erreur:" + err
+            };
+        }
+
+        createNotification({ message: "Locataire créé", status: 200, success: true });
+        throw redirect(303, Routes.tenants);
+    }
+};
---
+++ src/routes/(app)/tenants/new/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Clickable from '$lib/components/atoms/Clickable.svelte';
+	import FormForUser from '$lib/components/organisms/Forms/createUser.svelte';
+	import { Routes } from '$lib/constants/routes';
+</script>
+
+<section class="flex items-center justify-center w-full mt-10">
+	<div class="flex flex-col max-w-xl bg-white shadow-lg rounded-xl">
+		<div class="p-5 space-y-5 bg-indigo-500 rounded-t-xl">
+			<h2 class="text-3xl text-white font-hind">Création du locataire</h2>
+			<p class="text-base text-white font-hind">
+				Les informations ci-dessous seront nécessaires pour la génération des quittances.
+			</p>
+		</div>
+		<div class="p-5">
+			<form id="landlord" method="POST" class="space-y-5">
+				<FormForUser />
+				<div class="flex space-x-5">
+					<Clickable secondary likeButton href={Routes.tenants}>Annuler</Clickable>
+					<Clickable primary type="submit">Ajouter</Clickable>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
