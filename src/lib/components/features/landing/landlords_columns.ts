e3f45a392ca5d86d481c6138bad8ee5d0af6bf82
Rajanan <g.rajan1993@gmail.com>
2024-10-07 08:05:44 +0000

    const for landing apge

---
+++ src/lib/components/features/landing/landlords_columns.ts
@@ -0,0 +1,32 @@
+
+export const landlordsColumns: { header: string, dataIndex: string; }[] = [
+    {
+        header: 'Bailleur',
+        dataIndex: 'landlord'
+    },
+    {
+        header: 'Locataire',
+        dataIndex: 'tenant'
+    },
+    {
+        header: 'Adresse du local',
+        dataIndex: 'address'
+    },
+    {
+        header: 'Statut',
+        dataIndex: 'startDate'
+    },
+    {
+        header: 'Date de paiement',
+        dataIndex: 'paymentDate'
+    },
+    {
+        header: 'Total',
+        dataIndex: 'total'
+    },
+
+    {
+        header: 'Option',
+        dataIndex: ''
+    }
+];
