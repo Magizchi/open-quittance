1e51d3505462e3a207504831ba4aa3ce075d7059
Rajanan <g.rajan1993@gmail.com>
2024-10-09 20:02:03 +0000

    fix(atom:modal):can close modal from slot

--- src/lib/components/atoms/Modal.svelte
+++ src/lib/components/atoms/Modal.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import Clickable from './Clickable.svelte';
-
 	export let showModal: Boolean;
 	let dialog: HTMLDialogElement;
 
 	$: if (dialog && showModal) dialog.showModal();
+	$: if (dialog && !showModal) dialog.close();
 </script>
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
@@ -18,8 +17,5 @@
 >
 	<div class="relative">
 		<slot />
-		<!-- <div class="absolute top-0 right-0">
-			<Clickable on:click={() => dialog.close()}>X</Clickable>
-		</div> -->
 	</div>
 </dialog>
