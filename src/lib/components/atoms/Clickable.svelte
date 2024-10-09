c8afa6192cfbefe3f460e2a97ffd9a3793fbdf2d
Rajanan <g.rajan1993@gmail.com>
2024-10-09 08:32:40 +0000

    style(atom:clikable):test style

--- src/lib/components/atoms/Clickable.svelte
+++ src/lib/components/atoms/Clickable.svelte
@@ -9,22 +9,20 @@
 	export let success: boolean = false;
 	export let error: boolean = false;
 	export let warning: boolean = false;
+	export let border: boolean = false;
+	export let valide: boolean = false;
 </script>
 
 {#if href}
 	<a
 		{...$$restProps}
 		class="flex px-4 py-2 text-white rounded font-hind aria-[current=true]:bg-teal-500 aria-[current=true]:hover:bg-teal-600"
-		class:bg-indigo-500={primary}
-		class:hover:bg-indigo-600={primary}
-		class:bg-slate-500={secondary}
-		class:hover:bg-slate-600={secondary}
-		class:bg-teal-500={current || success}
-		class:hover:bg-teal-600={current || success}
-		class:bg-rose-500={error}
-		class:hover:bg-rose-600={error}
-		class:bg-amber-500={warning}
-		class:hover:bg-amber-600={warning}
+		class:primaryClass={primary}
+		class:secondaryClass={secondary}
+		class:successClass={current || success}
+		class:errorClass={error}
+		class:warningClass={warning}
+		class:borderClass={border}
 		class:justify-center={likeButton}
 		class:w-full={likeButton}
 		{href}
@@ -36,17 +34,56 @@
 		{...$$restProps}
 		on:click
 		class="w-full px-4 py-2 text-white rounded font-hind"
-		class:bg-indigo-500={primary}
-		class:hover:bg-indigo-600={primary}
-		class:bg-slate-500={secondary}
-		class:hover:bg-slate-600={secondary}
-		class:bg-teal-500={success}
-		class:hover:bg-teal-600={success}
-		class:bg-rose-500={error}
-		class:hover:bg-rose-600={error}
-		class:bg-amber-500={warning}
-		class:hover:bg-amber-600={warning}
+		class:primaryClass={primary}
+		class:secondaryClass={secondary}
+		class:successClass={success}
+		class:errorClass={error}
+		class:warningClass={warning}
+		class:borderClass={border}
+		class:valideClass={valide}
 	>
 		<slot />
 	</button>
 {/if}
+
+<style>
+	.valideClass {
+		@apply bg-teal-500 text-white;
+	}
+	.primaryClass {
+		@apply bg-indigo-500;
+	}
+	.primaryClass:hover {
+		@apply bg-indigo-600;
+	}
+	.secondaryClass {
+		@apply bg-slate-500;
+	}
+	.secondaryClass:hover {
+		@apply bg-slate-600;
+	}
+	.successClass {
+		@apply bg-teal-500;
+	}
+	.successClass:hover {
+		@apply bg-teal-600;
+	}
+	.warningClass {
+		@apply bg-amber-500;
+	}
+	.warningClass:hover {
+		@apply bg-amber-600;
+	}
+	.errorClass {
+		@apply bg-rose-500;
+	}
+	.errorClass:hover {
+		@apply bg-rose-600;
+	}
+	.borderClass {
+		@apply text-slate-400 border-2 border-slate-400;
+	}
+	.borderClass:hover {
+		@apply border-slate-500 text-slate-500 bg-slate-100;
+	}
+</style>
