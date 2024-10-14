adbb926f82919114186ebc834941f7e25471f7b1
Rajanan <g.rajan1993@gmail.com>
2024-10-14 12:22:47 +0000

    fix(page:login):eslint control

--- src/routes/(authentication)/login/+page.server.ts
+++ src/routes/(authentication)/login/+page.server.ts
@@ -1,60 +1,60 @@
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
 import { redirect, fail } from '@sveltejs/kit';
-import db from "$lib/server/database";
-import { usersTable } from "$lib/server/schema";
-import { eq } from "drizzle-orm";
-import { createCookie } from "$lib/utils/remember_me.js";
-import { Routes } from "$lib/constants/routes.js";
+import db from '$lib/server/database';
+import { usersTable } from '$lib/server/schema';
+import { eq } from 'drizzle-orm';
+import { createCookie } from '$lib/utils/remember_me.js';
+import { Routes } from '$lib/constants/routes.js';
 
 export const load = ({ cookies }) => {
-    const cookiesJwt = cookies.get('remember_me');
-    if (cookiesJwt) {
-        throw redirect(303, "/");
-    }
+	const cookiesJwt = cookies.get('remember_me');
+	if (cookiesJwt) {
+		throw redirect(303, Routes.landing);
+	}
 };
 
 export const actions = {
-    default: async ({ request, cookies }) => {
-        const data = await request.formData();
-        const login = data.get('username') || "";
-        const password = data.get('password') || "";
+	default: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const login = data.get('username') || '';
+		const password = data.get('password') || '';
 
-        if (!login || !password) {
-            return fail(403, {
-                message: "Donnée incorrect",
-                incorrect: true
-            });
-        }
+		if (!login || !password) {
+			return fail(403, {
+				message: 'Donnée incorrect',
+				incorrect: true
+			});
+		}
 
-        // Check user email
-        const [user] = await db.select({
-            id: usersTable.id,
-            email: usersTable.email,
-            hash: usersTable.password
-        })
-            .from(usersTable)
-            .where(eq(usersTable.email, login.toString())) as { id: number; email: string; hash: string; }[];
+		// Check user email
+		const [user] = (await db
+			.select({
+				id: usersTable.id,
+				email: usersTable.email,
+				hash: usersTable.password
+			})
+			.from(usersTable)
+			.where(eq(usersTable.email, login.toString())));
 
-        if (!user) {
-            return fail(404, {
-                message: "Identifiant ou Mot de passe incorrect",
-                incorrect: true
-            });
-        }
-        // Check password
-        const { hash, ...userInfo } = user;
-        const authenticated = await bcrypt.compare(password.toString(), hash);
+		if (!user) {
+			return fail(404, {
+				message: 'Identifiant ou Mot de passe incorrect',
+				incorrect: true
+			});
+		}
+		// Check password
+		const { hash, ...userInfo } = user;
+		const authenticated = await bcrypt.compare(password.toString(), hash);
 
-        if (!authenticated) {
-            return fail(401, {
-                message: "Identifiant ou Mot de passe incorrect",
-                incorrect: true
-            });
-        }
+		if (!authenticated) {
+			return fail(401, {
+				message: 'Identifiant ou Mot de passe incorrect',
+				incorrect: true
+			});
+		}
 
-        // Add Cookie "remember_me"
-        await createCookie(userInfo, cookies);
+		await createCookie(userInfo, cookies);
 
-        throw redirect(303, Routes.landing);
-    }
+		throw redirect(303, Routes.landing);
+	}
 };
