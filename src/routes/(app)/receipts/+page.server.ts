99e18184e03896df52dd305d2217785495e49aca
Rajanan <g.rajan1993@gmail.com>
2024-10-15 17:21:16 +0000

    delete receipts

--- src/routes/(app)/receipts/+page.server.ts
+++ src/routes/(app)/receipts/+page.server.ts
@@ -1,19 +1,22 @@
-import db from '$lib/server/database';
-import { receiptsTable } from '$lib/server/schema';
+import db from '$lib/db/drizzle';
+import { receiptsTable } from '$lib/db/schema';
 import { addPaymentDate } from '$lib/utils/service/receipts';
 import { desc } from 'drizzle-orm';
-import ReceiptsModel from '$lib/models/receipts.model.js';
+
 
 export async function load({ parent }) {
-    await parent();
+	await parent();
 
-    const receiptList: ReceiptsModel[] = await db.select().from(receiptsTable).orderBy(desc(receiptsTable.startDate));
+	const receiptList = await db
+		.select()
+		.from(receiptsTable)
+		.orderBy(desc(receiptsTable.startDate));
 
-    return { receiptList };
+	return { receiptList };
 }
 
 export const actions = {
-    paymentDate: async ({ request }) => {
-        return await addPaymentDate(request);
-    }
+	paymentDate: async ({ request }) => {
+		return await addPaymentDate(request);
+	}
 };
