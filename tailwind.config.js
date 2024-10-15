9961d69d4e48a9645591ca7414838acb0342c470
Rajanan <g.rajan1993@gmail.com>
2024-10-15 17:32:03 +0000

    config(tailwind):fix eslint err

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
+import TailwindForm from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'hind':['hind','base']
-      }
-    },
-  },
-  plugins: [
-    require("@tailwindcss/forms")({
-      strategy: 'base',
-    })
-  ],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				hind: ['hind', 'base']
+			}
+		}
+	},
+	plugins: [
+		TailwindForm({
+			strategy: 'base'
+		})
+	]
+};
