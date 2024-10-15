f49ace585645c169b03d393c09db5772d7ba2ac0
Rajanan <g.rajan1993@gmail.com>
2024-10-15 10:32:42 +0000

    feat(organism:Table):add pagination button

--- src/lib/components/organisms/Table/table.svelte
+++ src/lib/components/organisms/Table/table.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import { ArrowLeftIcon, ArrowRightIcon } from '$lib/components/atoms/Icons/icon';
 	import dayjs from 'dayjs';
 
@@ -10,10 +12,18 @@
 	export let columns: ColumnsProps[];
 	export let rows: any[] = [];
 	export const index: number = 0;
-	export let filterPage: (page: number, show: number) => void = () => {};
 	export let scalpe: boolean = false;
-	let page: number = 1;
-	let show: number = 12;
+
+	let currentPage: number = $page.url.searchParams.get('page')
+		? Number($page.url.searchParams.get('page'))
+		: 1;
+	let show: number = $page.url.searchParams.get('show')
+		? Number($page.url.searchParams.get('show'))
+		: 12;
+
+	let pagePagination: (currentPage: number, show: number) => void = (currentPage, show) => {
+		goto(`?page=${currentPage}&show=${show}`);
+	};
 </script>
 
 <div class="overflow-hidden border border-gray-200 rounded-lg shadow">
@@ -64,21 +74,21 @@
 	<div class="flex space-x-1">
 		<button
 			class="flex items-center justify-center p-1 text-4xl border rounded bg-slate-200 border-slate-300"
-			disabled={page === 1}
+			disabled={currentPage === 1}
 			on:click={() => {
-				if (page <= 1) {
-					return (page = 1);
+				if (currentPage <= 1) {
+					return (currentPage = 1);
 				}
-				page -= 1;
-				filterPage(page, show);
-				return page;
+				currentPage -= 1;
+				pagePagination(currentPage, show);
+				return currentPage;
 			}}><ArrowLeftIcon width="20" /></button
 		>
 		<input
 			name="pageNumber"
 			class="text-center border rounded w-9 border-slate-300"
 			class:bg-slate-200={rows.length <= show}
-			bind:value={page}
+			bind:value={currentPage}
 			disabled={rows.length <= show}
 			min={1}
 		/>
@@ -86,8 +96,8 @@
 			class="flex items-center justify-center p-1 text-4xl border rounded bg-slate-200 border-slate-300"
 			disabled={rows.length <= show}
 			on:click={() => {
-				page += 1;
-				filterPage(page, show);
+				currentPage += 1;
+				pagePagination(currentPage, show);
 			}}
 		>
 			<ArrowRightIcon width="20" />
@@ -95,11 +105,11 @@
 	</div>
 	<select
 		bind:value={show}
-		on:change={() => filterPage(page, show)}
+		on:change={() => pagePagination(currentPage, show)}
 		class="bg-white border rounded border-slate-300 focus:ring-indigo-500 font-hind text-slate-700"
 	>
-		<option value={12} selected class="font-hind text-slate-700">12/pages</option>
-		<option value={24} class="font-hind text-slate-700">24/pages</option>
-		<option value={36} class="font-hind text-slate-700">36/pages</option>
+		<option value={12} selected class="font-hind text-slate-700">12/page</option>
+		<option value={24} class="font-hind text-slate-700">24/page</option>
+		<option value={36} class="font-hind text-slate-700">36/page</option>
 	</select>
 </div>
