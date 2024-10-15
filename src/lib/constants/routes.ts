d0b3b722ce1d142258f17ea33087d509dcf3f993
Rajanan <g.rajan1993@gmail.com>
2024-10-15 10:33:15 +0000

    refaco(const:routes):change route name

--- src/lib/constants/routes.ts
+++ src/lib/constants/routes.ts
@@ -1,11 +1,11 @@
 export enum Routes {
-    login = '/login',
-    landing = '/',
-    createLandlords = '/landlords/create',
-    properties = '/properties',
-    createProperty = '/properties/new',
-    tenants = '/tenants',
-    createTenant = '/tenants/new',
-    rentals = '/rentals',
-    createRental = '/rentals/new'
-};
+	login = '/login',
+	landing = '/',
+	createLandlords = '/landlords/new',
+	properties = '/properties',
+	createProperty = '/properties/new',
+	tenants = '/tenants',
+	createTenant = '/tenants/new',
+	rentals = '/rentals',
+	createRental = '/rentals/new'
+}
