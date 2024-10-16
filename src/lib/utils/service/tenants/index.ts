2ce441ff371cc3d6f93d5e0c93ec2fd122129be6
Rajanan <g.rajan1993@gmail.com>
2024-10-16 08:56:29 +0000

    delete service rental;tenants

--- src/lib/utils/service/tenants/index.ts
+++
@@ -1,6 +0,0 @@
-import db from "$lib/server/database";
-import { tenantsTable } from "$lib/server/schema";
-
-export const GetTenants = (page: number = 1, show: number = 12) => {
-    return db.select().from(tenantsTable).limit(show).offset(show * (page - 1));
-};
