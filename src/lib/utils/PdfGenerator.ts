5cf03f1671276a2a0e59488ddcbd2ad4a4cdd83b
Rajanan <g.rajan1993@gmail.com>
2024-10-16 13:12:33 +0000

    fix:linter/eslint

--- src/lib/utils/PdfGenerator.ts
+++ src/lib/utils/PdfGenerator.ts
@@ -1,27 +1,29 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import BlobStream from "blob-stream";
-import type { IBlobStream } from "blob-stream";
+import BlobStream from 'blob-stream';
+import type { IBlobStream } from 'blob-stream';
 import PdfPrinter from 'pdfmake';
-import type { BufferOptions, TDocumentDefinitions } from "pdfmake/interfaces";
+import type { BufferOptions, TDocumentDefinitions } from 'pdfmake/interfaces';
 
-const GeneratePdf = (docDefinition: TDocumentDefinitions, options?: BufferOptions): Promise<Blob> => {
+const GeneratePdf = (
+	docDefinition: TDocumentDefinitions,
+	options?: BufferOptions
+): Promise<Blob> => {
+	const fonts = {
+		Roboto: {
+			// Only Roboto work? I used "Hind"
+			normal: 'src/lib/assets/fonts/hind/OTF/Hind-Regular.otf',
+			bold: 'src/lib/assets/fonts/hind/OTF/Hind-Bold.otf'
+		}
+	};
 
-    const fonts = {
-        Roboto: { // Only Roboto work? I used "Hind"
-            normal: 'src/lib/assets/fonts/Hind/OTF/Hind-Regular.otf',
-            bold: 'src/lib/assets/fonts/Hind/OTF/Hind-Bold.otf'
-        },
-    };
+	const printer = new PdfPrinter(fonts);
 
-    const printer = new PdfPrinter(fonts);
-
-    return new Promise((resolve) => {
-        const pdfDoc = printer.createPdfKitDocument(docDefinition, options);
-        pdfDoc.pipe(BlobStream()).on('finish', function (this: IBlobStream) {
-            resolve(this.toBlob('application/pdf'));
-        });
-        pdfDoc.end();
-    });
+	return new Promise((resolve) => {
+		const pdfDoc = printer.createPdfKitDocument(docDefinition, options);
+		pdfDoc.pipe(BlobStream()).on('finish', function (this: IBlobStream) {
+			resolve(this.toBlob('application/pdf'));
+		});
+		pdfDoc.end();
+	});
 };
 
 export default GeneratePdf;
