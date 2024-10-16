6f1db6c454b780ab92071cb6834bcf6e175474fc
Rajanan <g.rajan1993@gmail.com>
2024-10-16 19:04:29 +0000

    eslint/prettier

--- src/lib/components/atoms/Modal.svelte
+++ src/lib/components/atoms/Modal.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-	export let showModal: Boolean;
+	export let showModal: boolean = false;
 	let dialog: HTMLDialogElement;
 
 	$: if (dialog && showModal) dialog.showModal();
 	$: if (dialog && !showModal) dialog.close();
 </script>
 
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<!-- svelte-ignore a11y-click-events-have-key-events -->
 <dialog
 	bind:this={dialog}
 	on:close={() => (showModal = false)}
-	on:click|self={() => dialog.close()}
 	class="shadow-md rounded-2xl scroll-m-10 min-w-[476px]"
 	{...$$restProps}
 >
