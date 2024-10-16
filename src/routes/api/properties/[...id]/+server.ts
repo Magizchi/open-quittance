75a5c863dc6f0b255828b725e7a39cb01870dba3
Rajanan <g.rajan1993@gmail.com>
2024-10-16 16:40:23 +0000

    zeaze

--- src/routes/api/properties/[...id]/+server.ts
+++ src/routes/api/properties/[...id]/+server.ts
@@ -1,15 +1,14 @@
-import db from '$lib/server/database.js';
-import { propertiesTable } from '$lib/server/schema.js';
+import db from '$lib/db/drizzle.js';
+import { propertiesTable } from '$lib/db/schema.js';
 import { json } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 
-
 export async function DELETE({ params }) {
-    try {
-        console.log(params);
-        await db.delete(propertiesTable).where(eq(propertiesTable.id, +params.id));
-        return json({ message: 'Propriété supprimer', success: true }, { status: 201 });
-    } catch (err) {
-        return new Response(null, { status: 204 }); json({ err }, { status: 400 });
-    }
+	try {
+		console.log(params);
+		await db.delete(propertiesTable).where(eq(propertiesTable.id, +params.id));
+		return json({ message: 'Propriété supprimer', success: true }, { status: 201 });
+	} catch (err) {
+		return new Response(JSON.stringify(err), { status: 204 });
+	}
 }
