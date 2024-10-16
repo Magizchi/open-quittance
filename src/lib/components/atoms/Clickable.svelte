cad71ff61877a3091b9dc22841811eef744b9bb7
Rajanan <g.rajan1993@gmail.com>
2024-10-16 13:01:15 +0000

    style(atoms:clickable):add variant

--- src/lib/components/atoms/Clickable.svelte
+++ src/lib/components/atoms/Clickable.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-	// Link
 	export let href: string | null = null;
-	export let current: boolean = false;
-	export let likeButton: boolean = false;
-	// Button && Link
-	export let primary: boolean = false;
-	export let secondary: boolean = false;
-	export let success: boolean = false;
-	export let error: boolean = false;
-	export let warning: boolean = false;
-	export let border: boolean = false;
-	export let valide: boolean = false;
+	export let variant:
+		| 'default'
+		| 'menu'
+		| 'primary'
+		| 'secondary'
+		| 'success'
+		| 'warning'
+		| 'error' = 'default';
+	export let className: string | null = null;
 </script>
 
 {#if href}
 	<a
 		{...$$restProps}
-		class="flex px-4 py-2 text-white rounded font-hind aria-[current=true]:bg-teal-500 aria-[current=true]:hover:bg-teal-600 hover:bg-teal-500"
-		class:primaryClass={primary}
-		class:secondaryClass={secondary}
-		class:successClass={current || success}
-		class:errorClass={error}
-		class:warningClass={warning}
-		class:borderClass={border}
-		class:justify-center={likeButton}
-		class:w-full={likeButton}
+		class="flex px-4 py-2 rounded font-hind {className}"
+		class:defaultClass={variant === 'default'}
+		class:menuClass={variant === 'menu'}
+		class:primaryClass={variant === 'primary'}
+		class:secondaryClass={variant === 'secondary'}
+		class:successClass={variant === 'success'}
+		class:warningClass={variant === 'warning'}
+		class:errorClass={variant === 'error'}
 		{href}
 	>
 		<slot />
@@ -33,57 +30,39 @@
 	<button
 		{...$$restProps}
 		on:click
-		class="w-full px-4 py-2 text-white rounded font-hind"
-		class:primaryClass={primary}
-		class:secondaryClass={secondary}
-		class:successClass={success}
-		class:errorClass={error}
-		class:warningClass={warning}
-		class:borderClass={border}
-		class:valideClass={valide}
+		class="px-4 py-2 rounded font-hind w-full {className}"
+		class:defaultClass={variant === 'default'}
+		class:menuClass={variant === 'menu'}
+		class:primaryClass={variant === 'primary'}
+		class:secondaryClass={variant === 'secondary'}
+		class:successClass={variant === 'success'}
+		class:warningClass={variant === 'warning'}
+		class:errorClass={variant === 'error'}
 	>
 		<slot />
 	</button>
 {/if}
 
-<style>
-	.valideClass {
-		@apply bg-teal-500 text-white;
+<style lang="postcss">
+	.defaultClass {
+		@apply text-slate-400 border-slate-400 hover:border-slate-500 hover:text-slate-500 hover:bg-slate-100;
+	}
+	.menuClass {
+		@apply text-white aria-[current=true]:bg-teal-500 aria-[current=true]:hover:bg-teal-600 hover:bg-teal-500;
 	}
 	.primaryClass {
-		@apply bg-indigo-500;
-	}
-	.primaryClass:hover {
-		@apply bg-indigo-600;
+		@apply text-white bg-indigo-500 hover:bg-indigo-600;
 	}
 	.secondaryClass {
-		@apply bg-slate-500;
-	}
-	.secondaryClass:hover {
-		@apply bg-slate-600;
+		@apply text-white bg-slate-500 hover:bg-slate-600;
 	}
 	.successClass {
-		@apply bg-teal-500;
-	}
-	.successClass:hover {
-		@apply bg-teal-600;
+		@apply text-white bg-teal-500 hover:bg-teal-600;
 	}
 	.warningClass {
-		@apply bg-amber-500;
-	}
-	.warningClass:hover {
-		@apply bg-amber-600;
+		@apply text-white bg-amber-500 hover:bg-amber-600;
 	}
 	.errorClass {
-		@apply bg-rose-500;
-	}
-	.errorClass:hover {
-		@apply bg-rose-600;
-	}
-	.borderClass {
-		@apply text-slate-400 border-2 border-slate-400;
-	}
-	.borderClass:hover {
-		@apply border-slate-500 text-slate-500 bg-slate-100;
+		@apply text-white bg-rose-500 hover:bg-rose-600;
 	}
 </style>
