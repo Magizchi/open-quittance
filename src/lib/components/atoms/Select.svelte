3e66f938e4ae1adbe1390d4a1a85a264be0b169b
Rajanan <g.rajan1993@gmail.com>
2024-10-16 13:01:37 +0000

    refacto(atom:select):no description

--- src/lib/components/atoms/Select.svelte
+++ src/lib/components/atoms/Select.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	export let options: any;
-	export let value: any;
+	export let options: { label: string; value: number }[] = [];
+	export let value: number;
 	export let label: string;
 	export let name: string;
 </script>
@@ -10,12 +10,9 @@
 	<select {name} bind:value {...$$restProps} class="text-slate-700 font-hind">
 		<option value="" selected> choisir... </option>
 		{#each options as option}
-			<option value={option.value} disabled={option.description ? true : false}>
+			<option value={option.value}>
 				<div class="flex flex-col">
 					<span class="bg-red-400">{option.label}</span>
-					{#if option.description}
-						<span>{option.description}</span>
-					{/if}
 				</div>
 			</option>
 		{/each}
