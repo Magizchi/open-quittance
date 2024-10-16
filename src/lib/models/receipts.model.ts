7eaedfc84b3c21f2b75566cee7b927e321077b53
Rajanan <g.rajan1993@gmail.com>
2024-10-16 14:01:06 +0000

    refacto(model:receipts):use drizzle default type $inferSelect

--- src/lib/models/receipts.model.ts
+++ src/lib/models/receipts.model.ts
@@ -1,34 +1,5 @@
-// import type { receiptsTable } from "$lib/server/schema";
+import type { receiptsTable } from "$lib/db/schema";
 
-// type receiptTypeII = typeof receiptsTable.$inferSelect;
+type ReceiptType = typeof receiptsTable.$inferSelect;
 
-interface ReceiptModel {
-    id: number;
-    number: string | number,
-    landlord_id: string | number;
-    landlord_fullName: string;
-    landlord_address: string;
-    landlord_city: string;
-    landlord_postalCode: string;
-
-    tenant_id: string | number;
-    tenant_address: string;
-    tenant_city: string;
-    tenant_fullName: string;
-    tenant_postalCode: string;
-
-    rent: string | number;
-    condo_fees: string | number;
-    taxes: string | number;
-
-    property_address: string;
-    property_city: string;
-    property_postalCode: string;
-
-    paymentDate: null | string | Date,
-    startDate: null | string | Date;
-    endDate: null | string | Date;
-    createAt: null | string | Date;
-}
-
-export default interface ReceiptsModel extends ReceiptModel { }
+export default interface ReceiptsModel extends ReceiptType { }
