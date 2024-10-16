86ffe3ea6b624302e47aeaebf65b9255ce4c7605
Rajanan <g.rajan1993@gmail.com>
2024-10-16 13:56:51 +0000

    refacto(models:rental):user default type from $inferSelect type

--- src/lib/models/rental.model.ts
+++ src/lib/models/rental.model.ts
@@ -1,42 +1,14 @@
-// import type {rentalsTable} from '$lib/server/schema';
 
-// type rentalType = typeof rentalsTable.$inferSelect;
+import type { landlordsTable, propertiesTable, rentalsTable, tenantsTable } from "$lib/db/schema";
+
+type rentalType = typeof rentalsTable.$inferSelect;
+type tenantsType = typeof tenantsTable.$inferSelect;
+type propertiesType = typeof propertiesTable.$inferSelect;
+type landlordsType = typeof landlordsTable.$inferSelect;
 
 export default interface RentalModel {
-    rentals: {
-        id: number;
-        startedAt: Date;
-        endDate: Date | null;
-        tenant_id: number;
-        property_id: number;
-    };
-    tenants: {
-        id: number;
-        name: string;
-        siret: string | null;
-        address: string;
-        city: string;
-        postalCode: string;
-    };
-    landlords: {
-        id: number;
-        name: string;
-        siret: string | null;
-        address: string;
-        city: string;
-        postalCode: string;
-    },
-    properties: {
-        id: number,
-        name: string,
-        address: string;
-        city: string;
-        postalCode: string;
-        rent: number;
-        condo_fees: number;
-        taxes: number;
-    },
-    rentalId: number,
-    startAt: string | Date,
-    endDate: string | Date;
+	rentals: rentalType;
+	tenants: tenantsType;
+	landlords: landlordsType;
+	properties: propertiesType;
 }
