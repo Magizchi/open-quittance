8c12a909a093dbb9f535581c2b6ed16539ed74ce
Rajanan <g.rajan1993@gmail.com>
2024-10-18 09:29:45 +0000

    fix eslint/prettier

--- src/routes/(authentication)/logout/+page.server.ts
+++ src/routes/(authentication)/logout/+page.server.ts
@@ -1,10 +1,7 @@
 import { Routes } from '$lib/constants/routes.js';
 import { redirect } from '@sveltejs/kit';
 
-
-
 export const load = ({ cookies }) => {
-
-    cookies.delete('remember_me');
-    throw redirect(303, Routes.login);
+	cookies.delete('remember_me');
+	throw redirect(303, Routes.login);
 };
