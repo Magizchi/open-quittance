bd724fd6cd85544581227d63b4eeb4d260a9ab63
Rajanan <g.rajan1993@gmail.com>
2024-10-20 16:23:04 +0000

    refactor(layout&page:login): apply new prettier tabs

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,75 +1,79 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import Clickable from '$lib/components/atoms/Clickable.svelte';
-	import Balise from '$lib/components/atoms/notification/balise.svelte';
-	import { ROUTES } from '$lib/constants/routes';
-	import Toast from '$lib/components/atoms/notification/toast.svelte';
-	export let data;
+  import { page } from "$app/stores";
+  import Clickable from "$lib/components/atoms/Clickable.svelte";
+  import { ROUTES } from "$lib/constants/routes";
+  import Toast from "$lib/components/atoms/notification/toast.svelte";
+  export let data;
 </script>
 
 <div class="flex bg-slate-800">
-	<aside class="min-h-screen p-4 space-y-8 font-hind w-60">
-		<h1 class="text-2xl font-bold text-teal-500 md:text-3xl font-hind">
-			<a href="/">
-				OPEN <span class="text-amber-500">QUITTANCE</span>
-			</a>
-		</h1>
-		{#if !data.needCreateLandlords}
-			<nav class="flex flex-col justify-between h-5/6">
-				<ul class="space-y-3 grow">
-					<li class="w-full">
-						<Clickable
-							variant="menu"
-							href={ROUTES.landing}
-							aria-current={$page.url.pathname === `${ROUTES.landing}`}>Quittances</Clickable
-						>
-					</li>
-					<li class="w-full space-y-2">
-						<Clickable
-							variant="menu"
-							href={ROUTES.properties}
-							aria-current={$page.url.pathname.includes(ROUTES.properties)}>Propriétés</Clickable
-						>
-					</li>
-					<li>
-						<Clickable
-							variant="menu"
-							href={ROUTES.tenants}
-							aria-current={$page.url.pathname.includes(`${ROUTES.tenants}`)}>Locataires</Clickable
-						>
-					</li>
-					<li>
-						<Clickable
-							variant="menu"
-							href={ROUTES.rentals}
-							aria-current={$page.url.pathname.includes(`${ROUTES.rentals}`)}>Locations</Clickable
-						>
-					</li>
-				</ul>
-				<ul class="space-y-3">
-					<li>
-						<Clickable
-							variant="menu"
-							href={ROUTES.settings}
-							aria-current={$page.url.pathname.includes(`${ROUTES.settings}`)}>Paramètre</Clickable
-						>
-					</li>
-					<li>
-						<Clickable
-							variant="menu"
-							href={ROUTES.logout}
-							aria-current={$page.url.pathname.includes(`${ROUTES.logout}`)}>Déconnexion</Clickable
-						>
-					</li>
-				</ul>
-			</nav>
-		{/if}
-	</aside>
-	<div class="flex flex-col w-full m-2 bg-slate-100 rounded-xl">
-		<main class="relative mt-5">
-			<Balise />
-			<Toast />
-			<slot class="z-0" />
-		</main>
-	</div>
+  <aside class="min-h-screen p-4 space-y-8 font-hind w-60">
+    <h1 class="text-2xl font-bold text-teal-500 md:text-3xl font-hind">
+      <a href="/">
+        OPEN <span class="text-amber-500">QUITTANCE</span>
+      </a>
+    </h1>
+    {#if !data.needCreateLandlords}
+      <nav class="flex flex-col justify-between h-5/6">
+        <ul class="space-y-3 grow">
+          <li class="w-full">
+            <Clickable
+              variant="menu"
+              href={ROUTES.landing}
+              aria-current={$page.url.pathname === `${ROUTES.landing}`}
+              >Quittances</Clickable
+            >
+          </li>
+          <li class="w-full space-y-2">
+            <Clickable
+              variant="menu"
+              href={ROUTES.properties}
+              aria-current={$page.url.pathname.includes(ROUTES.properties)}
+              >Propriétés</Clickable
+            >
+          </li>
+          <li>
+            <Clickable
+              variant="menu"
+              href={ROUTES.tenants}
+              aria-current={$page.url.pathname.includes(`${ROUTES.tenants}`)}
+              >Locataires</Clickable
+            >
+          </li>
+          <li>
+            <Clickable
+              variant="menu"
+              href={ROUTES.rentals}
+              aria-current={$page.url.pathname.includes(`${ROUTES.rentals}`)}
+              >Locations</Clickable
+            >
+          </li>
+        </ul>
+        <ul class="space-y-3">
+          <li>
+            <Clickable
+              variant="menu"
+              href={ROUTES.settings}
+              aria-current={$page.url.pathname.includes(`${ROUTES.settings}`)}
+              >Paramètre</Clickable
+            >
+          </li>
+          <li>
+            <Clickable
+              variant="menu"
+              href={ROUTES.logout}
+              aria-current={$page.url.pathname.includes(`${ROUTES.logout}`)}
+              >Déconnexion</Clickable
+            >
+          </li>
+        </ul>
+      </nav>
+    {/if}
+  </aside>
+  <div class="flex flex-col w-full m-2 bg-slate-100 rounded-xl">
+    <main class="relative mt-5">
+      <Toast />
+      <slot class="z-0" />
+    </main>
+  </div>
 </div>
--- src/routes/(authentication)/login/+page.svelte
+++ src/routes/(authentication)/login/+page.svelte
@@ -1,61 +1,74 @@
 <script lang="ts">
-	export let form;
-	import Input from '$lib/components/atoms/Input.svelte';
-	import logo from '$lib/assets/image/machine_quittance.jpg';
-	import Clickable from '$lib/components/atoms/Clickable.svelte';
+  import Input from "$lib/components/atoms/Input.svelte";
+  import logo from "$lib/assets/image/machine_quittance.jpg";
+  import Clickable from "$lib/components/atoms/Clickable.svelte";
 
-	const setMessage = () => {
-		form = { message: '', incorrect: false };
-	};
+  const setMessage = () => {
+    form = { message: "", incorrect: false };
+  };
+
+  export let form;
 </script>
 
-<section class="relative flex items-center justify-center h-screen bg-slate-200">
-	<div class="absolute inset-0 w-full h-full bg-amber-500 clip-path-diagonal" />
-	<div
-		class="z-20 w-full flex flex-row justify-center max-w-[1280px] min-h-[620px] border-2 shadow-sm border-indigo-500 rounded-xl m-10"
-	>
-		<div
-			class="flex items-center justify-center w-full md:w-1/2 bg-slate-100 rounded-xl md:rounded-none md:rounded-l-xl"
-		>
-			<div class="flex flex-col items-center w-full">
-				<div class="flex flex-col items-center mb-10 space-y-1">
-					<h1 class="text-2xl font-bold text-indigo-500 md:text-3xl font-hind">
-						OPEN <span class="text-amber-500">QUITTANCE</span>
-					</h1>
-					<p class="text-sm text-center text-slate-700 md:text-base">
-						Simplifiez la gestion et la génération de vos quittances de loyer en quelques clics.
-					</p>
-				</div>
-				<form class="w-1/2" method="post">
-					<div class="space-y-5">
-						<Input id="username" name="username" placeholder="Email ou identifiant" label="Email" />
-						<Input
-							id="password"
-							name="password"
-							type="password"
-							placeholder="Mots de passe"
-							on:change={() => setMessage()}
-							label="Mot de passe"
-						/>
-						<p class="text-sm font-hind text-rose-500 md:text-base">
-							{#if form}{form.message}{/if}
-						</p>
+<section
+  class="relative flex items-center justify-center h-screen bg-slate-200"
+>
+  <div class="absolute inset-0 w-full h-full bg-amber-500 clip-path-diagonal" />
+  <div
+    class="z-20 w-full flex flex-row justify-center max-w-[1280px] min-h-[620px] border-2 shadow-sm border-indigo-500 rounded-xl m-10"
+  >
+    <div
+      class="flex items-center justify-center w-full md:w-1/2 bg-slate-100 rounded-xl md:rounded-none md:rounded-l-xl"
+    >
+      <div class="flex flex-col items-center w-full">
+        <div class="flex flex-col items-center mb-10 space-y-1">
+          <h1 class="text-2xl font-bold text-indigo-500 md:text-3xl font-hind">
+            OPEN <span class="text-amber-500">QUITTANCE</span>
+          </h1>
+          <p class="text-sm text-center text-slate-700 md:text-base">
+            Simplifiez la gestion et la génération de vos quittances de loyer en
+            quelques clics.
+          </p>
+        </div>
+        <form class="w-1/2" method="post">
+          <div class="space-y-5">
+            <Input
+              id="username"
+              name="username"
+              placeholder="Email ou identifiant"
+              label="Email"
+            />
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Mots de passe"
+              on:change={() => setMessage()}
+              label="Mot de passe"
+            />
+            <p class="text-sm font-hind text-rose-500 md:text-base">
+              {#if form}{form.message}{/if}
+            </p>
 
-						<div class="w-full md:flex md:justify-end">
-							<Clickable variant="primary" type="submit">Connection</Clickable>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-		<div class="hidden w-1/2 rounded-r-xl md:block">
-			<img alt="logo" src={logo} class="w-full h-full bg-contain rounded-r-xl" />
-		</div>
-	</div>
+            <div class="w-full md:flex md:justify-end">
+              <Clickable variant="primary" type="submit">Connection</Clickable>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="hidden w-1/2 rounded-r-xl md:block">
+      <img
+        alt="logo"
+        src={logo}
+        class="w-full h-full bg-contain rounded-r-xl"
+      />
+    </div>
+  </div>
 </section>
 
 <style>
-	.clip-path-diagonal {
-		clip-path: polygon(0% 100%, 100% 0%, 100% 100%);
-	}
+  .clip-path-diagonal {
+    clip-path: polygon(0% 100%, 100% 0%, 100% 100%);
+  }
 </style>
