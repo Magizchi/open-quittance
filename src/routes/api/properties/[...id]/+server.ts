8b31a9e10479c879a7571df60674f3aa137cba53
Rajanan <g.rajan1993@gmail.com>
2024-10-22 13:58:21 +0000

    feat(api:properties): add methode delete properties

--- src/routes/api/properties/[...id]/+server.ts
+++ src/routes/api/properties/[...id]/+server.ts
@@ -1,14 +1,20 @@
-import db from '$lib/db/drizzle.js';
-import { propertiesTable } from '$lib/db/schema.js';
-import { json } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
+import db from "$lib/db/drizzle.js";
+import { propertiesTable } from "$lib/db/schema.js";
+import { json } from "@sveltejs/kit";
+import { eq } from "drizzle-orm";
 
 export async function DELETE({ params }) {
-	try {
-		console.log(params);
-		await db.delete(propertiesTable).where(eq(propertiesTable.id, +params.id));
-		return json({ message: 'Propriété supprimer', success: true }, { status: 201 });
-	} catch (err) {
-		return new Response(JSON.stringify(err), { status: 204 });
-	}
+  try {
+    await db.delete(propertiesTable).where(eq(propertiesTable.id, +params.id));
+    const response = await db.select().from(propertiesTable);
+    return json(
+      { message: "Propriété supprimer", success: true, data: response },
+      { status: 201 }
+    );
+  } catch (err) {
+    return json(
+      { message: "Propriété supprimer", success: false, data: [] },
+      { status: 400 }
+    );
+  }
 }
