08fe5b30a122728927160060c5d78ab7c067dec0
Rajanan <g.rajan1993@gmail.com>
2024-10-22 08:11:57 +0000

    fix(cookies): add path for cookie delete

--- src/routes/(authentication)/logout/+page.server.ts
+++ src/routes/(authentication)/logout/+page.server.ts
@@ -1,7 +1,7 @@
-import { ROUTES } from '$lib/constants/routes.js';
-import { redirect } from '@sveltejs/kit';
+import { ROUTES } from "$lib/constants/routes.js";
+import { redirect } from "@sveltejs/kit";
 
 export const load = ({ cookies }) => {
-	cookies.delete('remember_me');
-	throw redirect(303, ROUTES.login);
+  cookies.delete("remember_me", { path: "/" });
+  throw redirect(303, ROUTES.login);
 };
