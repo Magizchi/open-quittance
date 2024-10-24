5ca143980d6496d9d1cea1d74969fc9a5f4076c6
Rajanan <g.rajan1993@gmail.com>
2024-10-24 07:59:18 +0000

    feat(page:rentals/new): add toast, improve options for the select

--- src/routes/(app)/rentals/new/+page.server.ts
+++ src/routes/(app)/rentals/new/+page.server.ts
@@ -1,45 +1,65 @@
-import FormDataToJson from '$lib/utils/FormDataToJson.js';
-import { redirect } from '@sveltejs/kit';
-import db from '$lib/db/drizzle';
-import { propertiesTable, rentalsTable, tenantsTable } from '$lib/db/schema.js';
-import dayjs from 'dayjs';
-import { eq } from 'drizzle-orm';
+import FormDataToJson from "$lib/utils/FormDataToJson.js";
+import db from "$lib/db/drizzle";
+import { propertiesTable, rentalsTable, tenantsTable } from "$lib/db/schema.js";
+import dayjs from "dayjs";
+import { desc, eq, isNotNull, isNull, or } from "drizzle-orm";
 
 export const load = async ({ parent }) => {
-	await parent();
-	const propertiesOptions = (
-		await db
-			.select()
-			.from(propertiesTable)
-			.leftJoin(rentalsTable, eq(propertiesTable.id, rentalsTable.property_id))
-	).map((element) => ({ label: element.properties.name, value: element.properties.id }));
-	const tenantsOptions = (await db.select().from(tenantsTable)).map((element) => ({
-		label: element.name,
-		value: element.id
-	}));
+  await parent();
+  const propertiesOptions = (
+    await db
+      .selectDistinct()
+      .from(propertiesTable)
+      .leftJoin(rentalsTable, eq(propertiesTable.id, rentalsTable.property_id))
+      .orderBy(desc(rentalsTable.id))
+      .limit(1)
+      .where(or(isNull(rentalsTable.id), isNotNull(rentalsTable.endDate)))
+  ).map((element) => ({
+    label: element.properties.name,
+    value: element.properties.id,
+  }));
 
-	return { propertiesOptions, tenantsOptions };
+  const tenantsOptions = (
+    await db
+      .selectDistinct()
+      .from(tenantsTable)
+      .leftJoin(rentalsTable, eq(tenantsTable.id, rentalsTable.tenant_id))
+      .orderBy(desc(rentalsTable.id))
+      .limit(1)
+      .where(or(isNull(rentalsTable.id), isNotNull(rentalsTable.endDate)))
+  ).map((element) => ({
+    label: element.tenants.name,
+    value: element.tenants.id,
+  }));
+
+  return { propertiesOptions, tenantsOptions };
 };
 
 export const actions = {
-	default: async ({ request }) => {
-		const data = await request.formData();
-		const { property, tenant, date } = FormDataToJson(data);
-
-		try {
-			await db.insert(rentalsTable).values({
-				property_id: +property,
-				tenant_id: +tenant,
-				startedAt: dayjs(date.toString()).toDate()
-			});
-		} catch (err) {
-			return {
-				message: 'err' + err,
-				success: false,
-				status: 400
-			};
-		}
-
-		throw redirect(303, '/');
-	}
+  default: async ({ request }) => {
+    const data = await request.formData();
+    const { property, tenant, date } = FormDataToJson(data);
+    if (property === "" || tenant === "" || date === "") {
+      return {
+        message: "Erreur dans les données",
+        success: false,
+      };
+    }
+    try {
+      await db.insert(rentalsTable).values({
+        property_id: +property,
+        tenant_id: +tenant,
+        startedAt: dayjs(date.toString()).toDate(),
+      });
+      return {
+        message: "Location créé",
+        success: true,
+      };
+    } catch (err) {
+      return {
+        message: "Une erreur c'est produite lors de création de la location",
+        success: false,
+      };
+    }
+  },
 };
--- src/routes/(app)/rentals/new/+page.svelte
+++ src/routes/(app)/rentals/new/+page.svelte
@@ -1,29 +1,54 @@
-<script>
-	import Clickable from '$lib/components/atoms/Clickable.svelte';
-	import { ROUTES } from '$lib/constants/routes';
-	import FormRental from '../rentalForm.svelte';
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import { goto } from "$app/navigation";
+  import Clickable from "$lib/components/atoms/Clickable.svelte";
+  import { addToast } from "$lib/components/atoms/notification/createNotification.store.js";
+  import { ROUTES } from "$lib/constants/routes";
+  import FormRental from "../rentalForm.svelte";
 
-	export let data;
+  export let data;
+  export let form;
+  $: if (form) {
+    if (form.success) {
+      addToast.success(form.message, 5000);
+      goto(ROUTES.rentals);
+    } else {
+      addToast.alert(form.message, 5000);
+    }
+  }
 </script>
 
 <section class="flex items-center justify-center w-full mt-10">
-	<div class="flex flex-col max-w-xl bg-white shadow-lg rounded-xl">
-		<div class="p-5 space-y-5 bg-indigo-500 rounded-t-xl">
-			<h2 class="text-3xl text-white font-hind">Création d'une location</h2>
-			<p class="text-base text-white font-hind">
-				Les informations ci-dessous seront nécessaires pour la génération des quittances.
-			</p>
-		</div>
-		<div class="p-5">
-			<form id="landlord" method="POST" class="space-y-5">
-				<FormRental properties={data.propertiesOptions} tenants={data.tenantsOptions} />
-				<div class="flex space-x-5">
-					<Clickable variant="secondary" className="w-full justify-center" href={ROUTES.rentals}
-						>Annuler</Clickable
-					>
-					<Clickable variant="primary" type="submit">Ajouter</Clickable>
-				</div>
-			</form>
-		</div>
-	</div>
+  <div class="flex flex-col max-w-xl bg-white shadow-lg rounded-xl">
+    <div class="p-5 space-y-5 bg-indigo-500 rounded-t-xl">
+      <h2 class="text-3xl text-white font-hind">Création d'une location</h2>
+      <p class="text-base text-white font-hind">
+        Les informations ci-dessous seront nécessaires pour la génération des
+        quittances.
+      </p>
+    </div>
+    <div class="p-5">
+      <form
+        id="landlord"
+        method="POST"
+        class="space-y-5"
+        on:submit|preventDefault
+        use:enhance
+        action={ROUTES.createRental}
+      >
+        <FormRental
+          properties={data.propertiesOptions}
+          tenants={data.tenantsOptions}
+        />
+        <div class="flex space-x-5">
+          <Clickable
+            variant="secondary"
+            className="w-full justify-center"
+            href={ROUTES.rentals}>Annuler</Clickable
+          >
+          <Clickable variant="primary" type="submit">Ajouter</Clickable>
+        </div>
+      </form>
+    </div>
+  </div>
 </section>
