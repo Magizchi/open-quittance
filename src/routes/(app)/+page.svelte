29572f369f4615a9e54aa607aaab5d4c512edeaa
Rajanan <g.rajan1993@gmail.com>
2024-10-24 09:30:14 +0000

    refactor(page:landindpage): delete payment date column and add paymeny date in badge

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,142 +1,134 @@
 <script lang="ts">
-	import { saveAs } from 'file-saver';
-	import { formatDate, toDay } from '$lib/utils/date.js';
-	import { PdfIcon } from '$lib/components/atoms/Icons/icon.js';
-	import { Table, Tr, Td } from '$lib/components/organisms/Table';
-	import Modal from '$lib/components/atoms/Modal.svelte';
-	import Clickable from '$lib/components/atoms/Clickable.svelte';
-	import ValideReceipt from '$lib/components/organisms/Forms/valideReceipt.svelte';
-	import Badge from '$lib/components/atoms/Badge.svelte';
-	import type { ReceiptsModel } from '$lib/models';
+  import { saveAs } from "file-saver";
+  import { formatDate, toDay } from "$lib/utils/date.js";
+  import { PdfIcon } from "$lib/components/atoms/Icons/icon.js";
+  import { Table, Tr, Td } from "$lib/components/organisms/Table";
+  import Modal from "$lib/components/atoms/Modal.svelte";
+  import Clickable from "$lib/components/atoms/Clickable.svelte";
+  import ValideReceipt from "$lib/components/organisms/Forms/valideReceipt.svelte";
+  import Badge from "$lib/components/atoms/Badge.svelte";
+  import type { ReceiptsModel } from "$lib/models";
 
-	export let data;
+  export let data;
 
-	const receiptsColumns: { header: string; dataIndex: string }[] = [
-		{
-			header: 'Locataire',
-			dataIndex: 'tenant'
-		},
-		{
-			header: 'Adresse du local',
-			dataIndex: 'address'
-		},
-		{
-			header: 'Statut',
-			dataIndex: 'startDate'
-		},
-		{
-			header: 'Date de paiement',
-			dataIndex: 'paymentDate'
-		},
-		{
-			header: 'Total',
-			dataIndex: 'total'
-		},
-		{
-			header: '',
-			dataIndex: ''
-		}
-	];
+  const receiptsColumns: { header: string; dataIndex: string }[] = [
+    {
+      header: "Locataire",
+      dataIndex: "tenant",
+    },
+    {
+      header: "Adresse du local",
+      dataIndex: "address",
+    },
+    {
+      header: "Statut",
+      dataIndex: "startDate",
+    },
+    {
+      header: "Total",
+      dataIndex: "total",
+    },
+    {
+      header: "",
+      dataIndex: "",
+    },
+  ];
 
-	let pdfBuffer: any;
-	let showModalPaymentDate: boolean = false;
-	let selectedReceipts: ReceiptsModel = {
-		startDate: toDay(),
-		id: 0
-	} as ReceiptsModel;
+  let pdfBuffer: Blob;
+  let showModalPaymentDate: boolean = false;
+  let selectedReceipts: ReceiptsModel = {
+    startDate: toDay(),
+    id: 0,
+  } as ReceiptsModel;
 
-	let documentName: string;
-	async function getPdf(receiptId: number) {
-		const response = await fetch(`/api/generate-pdf?receiptId=${receiptId}`)
-			.then((res) => {
-				const getDocumentName = res.headers.get('document-name');
-				if (getDocumentName) {
-					documentName = getDocumentName;
-				}
-				return res;
-			})
-			.then((data) => data.blob());
+  let documentName: string;
+  async function getPdf(receiptId: number) {
+    const response = await fetch(`/api/generate-pdf?receiptId=${receiptId}`)
+      .then((res) => {
+        const getDocumentName = res.headers.get("document-name");
+        if (getDocumentName) {
+          documentName = getDocumentName;
+        }
+        return res;
+      })
+      .then((data) => data.blob());
 
-		pdfBuffer = response;
+    pdfBuffer = response;
 
-		const blob = new Blob([pdfBuffer], { type: 'application/pdf' });
+    const blob = new Blob([pdfBuffer], { type: "application/pdf" });
 
-		saveAs(blob, `quittance-${documentName}.pdf`);
-	}
+    saveAs(blob, `quittance-${documentName}.pdf`);
+  }
 </script>
 
 <section class="relative px-10 space-y-5 bg-slate-100">
-	<h1 class="text-2xl font-bold font-hind text-slate-700">Tableau de bord</h1>
-	<Table scalpe columns={receiptsColumns} rows={data.receiptList} let:row>
-		<Tr>
-			{#if typeof row === 'string'}
-				<td colspan={receiptsColumns.length} class="font-bold bg-gray-300 text-slate-700 font-hind"
-					>{row}</td
-				>
-			{:else}
-				<Td>{row.tenant_fullName}</Td>
-				<Td>
-					<div class="flex flex-col items-end text-justify">
-						{row.property_address}
-						<span class="flex space-x-2">
-							{row.property_city}
-							{row.property_postalCode}
-						</span>
-					</div>
-				</Td>
-				<Td>
-					{#if row.paymentDate}
-						<Badge valide>Payé</Badge>
-					{:else}
-						<Badge error>Reste à payer</Badge>
-					{/if}
-				</Td>
-				<Td>
-					{#if row.paymentDate}
-						<p>{formatDate(row.paymentDate)}</p>
-					{:else}
-						<p>/</p>
-					{/if}
-				</Td>
-				<Td>
-					{row.rent + row.condo_fees + row.taxes}
-				</Td>
-				<Td>
-					<div class="flex items-center justify-between w-full space-x-5">
-						<Clickable
-							variant="secondary"
-							on:click={() => {
-								getPdf(row.id);
-							}}
-						>
-							<div class="flex flex-row items-center justify-center">
-								<PdfIcon class="text-base" height="20" />
-								<span class="text-white font-hind">PDF</span>
-							</div>
-						</Clickable>
-						{#if !row.paymentDate}
-							<Clickable
-								variant="border"
-								on:click={() => {
-									showModalPaymentDate = true;
-									selectedReceipts = row;
-								}}
-							>
-								Valider
-							</Clickable>
-						{/if}
-					</div>
-				</Td>
-			{/if}
-		</Tr>
-	</Table>
+  <h1 class="text-2xl font-bold font-hind text-slate-700">Tableau de bord</h1>
+  <Table scalpe columns={receiptsColumns} rows={data.receiptList} let:row>
+    <Tr>
+      {#if typeof row === "string"}
+        <td
+          colspan={receiptsColumns.length}
+          class="font-bold bg-gray-300 text-slate-700 font-hind">{row}</td
+        >
+      {:else}
+        <Td>{row.tenant_fullName}</Td>
+        <Td>
+          <div class="flex flex-col items-end text-justify">
+            {row.property_address}
+            <span class="flex space-x-2">
+              {row.property_city}
+              {row.property_postalCode}
+            </span>
+          </div>
+        </Td>
+        <Td>
+          {#if row.paymentDate}
+            <Badge valide>Payé {formatDate(row.paymentDate)}</Badge>
+          {:else}
+            <Badge error>Reste à payer</Badge>
+          {/if}
+        </Td>
+        <Td>
+          {row.rent + row.condo_fees + row.taxes}
+        </Td>
+        <Td>
+          <div class="flex items-center justify-between w-full pl-10 space-x-5">
+            <Clickable
+              variant="secondary"
+              on:click={() => {
+                getPdf(row.id);
+              }}
+            >
+              <div class="flex flex-row items-center justify-center">
+                <PdfIcon class="text-base" height="20" />
+                <span class="text-white font-hind">PDF</span>
+              </div>
+            </Clickable>
+            {#if !row.paymentDate}
+              <Clickable
+                variant="border"
+                on:click={() => {
+                  showModalPaymentDate = true;
+                  selectedReceipts = row;
+                }}
+              >
+                Valider
+              </Clickable>
+            {/if}
+          </div>
+        </Td>
+      {/if}
+    </Tr>
+  </Table>
 </section>
 
 <Modal bind:showModal={showModalPaymentDate}>
-	<form id="AddPaymentDate" method="POST" action="?/paymentDate">
-		<ValideReceipt {selectedReceipts} />
-	</form>
-	<div class="flex justify-end px-10 py-3">
-		<Clickable primary form="AddPaymentDate" type="submit">Sauvegarder</Clickable>
-	</div>
+  <form id="AddPaymentDate" method="POST" action="?/paymentDate">
+    <ValideReceipt {selectedReceipts} />
+  </form>
+  <div class="flex justify-end px-10 py-3">
+    <Clickable primary form="AddPaymentDate" type="submit"
+      >Sauvegarder</Clickable
+    >
+  </div>
 </Modal>
