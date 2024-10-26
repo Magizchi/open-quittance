d252cb397742f157cb2ea58e8782f2ed90ff6eed
Rajanan <g.rajan1993@gmail.com>
2024-10-26 12:09:37 +0000

    feat(page:settings): form update users, link to landlord form

---
+++ src/routes/(app)/settings/+page.server.ts
@@ -0,0 +1,88 @@
+import db from "$lib/db/drizzle.js";
+import { landlordsTable, usersTable } from "$lib/db/schema.js";
+import FormDataToJson from "$lib/utils/FormDataToJson.js";
+import { fail } from "@sveltejs/kit";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
+import checkCookie, { createCookie } from "$lib/utils/remember_me.js";
+
+export const load = async ({ parent }) => {
+  await parent();
+
+  const [landlord] = await db.select().from(landlordsTable);
+  return { landlord };
+};
+
+export const actions = {
+  default: async ({ request, cookies }) => {
+    const data = await request.formData();
+    const userCookie = checkCookie(cookies);
+    const { email, currentPassword, newPassword, confirmPassword } =
+      FormDataToJson(data);
+
+    if (!userCookie) {
+      return fail(401, {
+        success: false,
+        message: "Vous devez être connecté",
+      });
+    }
+
+    if (email === "" || currentPassword === "") {
+      return fail(404, {
+        success: false,
+        message: "Email ou Mot de passe doit être remplis",
+      });
+    }
+
+    // Check user email
+    const [user] = await db
+      .select({
+        id: usersTable.id,
+        email: usersTable.email,
+        hash: usersTable.password,
+      })
+      .from(usersTable)
+      .where(eq(usersTable.loginToken, userCookie.loginToken));
+
+    if (!user) {
+      return fail(404, {
+        message: "Identifiant ou Mot de passe incorrect",
+        incorrect: true,
+      });
+    }
+
+    const { hash } = user;
+    const authenticated = bcrypt.compareSync(currentPassword.toString(), hash);
+
+    if (!authenticated) {
+      return fail(404, {
+        message: "Identifiant ou Mot de passe incorrect",
+        incorrect: true,
+      });
+    }
+
+    if (newPassword !== confirmPassword) {
+      return fail(404, {
+        message: "Le nouveau mot de passe ne correspond pas",
+        incorrect: true,
+      });
+    }
+
+    const hashedPassword = bcrypt.hashSync(newPassword, bcrypt.genSaltSync());
+
+    try {
+      await db.update(usersTable).set({
+        ...user,
+        email,
+        password: newPassword ? hashedPassword : user.hash,
+      });
+
+      await createCookie({ ...user, email }, cookies);
+    } catch {
+      return fail(404, {
+        message: "Une erreur c'est produite",
+        incorrect: true,
+      });
+    }
+  },
+};
---
+++ src/routes/(app)/settings/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Clickable from "$lib/components/atoms/Clickable.svelte";
+  import Input from "$lib/components/atoms/Input.svelte";
+  import { ROUTES } from "$lib/constants/routes";
+  export let data;
+</script>
+
+<h1 class="mb-5 ml-10 text-2xl font-bold font-hind text-slate-700">
+  Paramètre
+</h1>
+<section class="max-w-lg mb-10 ml-10 space-y-5">
+  <h2 class="text-xl font-hind text-slate-700">Utilisateur</h2>
+  <form id="user" method="POST" class="space-y-2 w-96">
+    <Input
+      label="Email"
+      name="email"
+      required
+      type="text"
+      placeholder="Votre email"
+    />
+
+    <Input
+      label="Mot de passe actuel"
+      name="currentPassword"
+      required
+      type="password"
+    />
+    <Input label="Nouveau mots de passe" name="newPassword" type="password" />
+    <Input
+      label="Confirmer votre mot de passe"
+      name="confirmPassword"
+      type="password"
+    />
+    <Clickable className="w-96" type="submit" variant="primary"
+      >Mettre à jour</Clickable
+    >
+  </form>
+</section>
+<section class="max-w-lg mb-10 ml-10 space-y-5">
+  <h2 class="text-xl font-hind text-slate-700">Bailleur</h2>
+  <div class=" font-hind text-slate-700">
+    {data.landlord.name}
+    {data.landlord.address}
+    {data.landlord.city}
+    {data.landlord.postalCode}
+  </div>
+  <Clickable
+    href={ROUTES.landlord.replace("{id}", data.landlord.id.toString())}
+    variant="primary"
+    className="w-96">Modifier Bailleur</Clickable
+  >
+</section>
