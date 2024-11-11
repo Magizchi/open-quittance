8b12ce0f99a24d82f877d0278301f6aeb1396e21
Rajanan <g.rajan1993@gmail.com>
2024-11-11 15:42:25 +0000

    fix options in select

--- src/lib/components/atoms/Select.svelte
+++ src/lib/components/atoms/Select.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
-	export let options: { label: string; value: number }[] = [];
-	export let value: number;
-	export let label: string;
-	export let name: string;
+  export let options: { label: string; value: number }[] = [];
+  export let value: number;
+  export let label: string;
+  export let name: string;
 </script>
 
 <div class="flex flex-col space-y-2">
-	<label class="text-slate-700 font-hind" for={'select' + name}>{label}</label>
-	<select {name} bind:value {...$$restProps} class="text-slate-700 font-hind">
-		<option value="" selected> choisir... </option>
-		{#each options as option}
-			<option value={option.value}>
-				<div class="flex flex-col">
-					<span class="bg-red-400">{option.label}</span>
-				</div>
-			</option>
-		{/each}
-	</select>
+  <label class="text-slate-700 font-hind" for={"select" + name}>{label}</label>
+  <select {name} bind:value {...$$restProps} class="text-slate-700 font-hind">
+    <option value="" selected> choisir... </option>
+    {#each options as option}
+      <option value={option.value}>
+        {option.label}
+      </option>
+    {/each}
+  </select>
 </div>
