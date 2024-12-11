85fd58312f7db12d8732ae021024f2b9202fb8e2
Rajanan <g.rajan1993@gmail.com>
2024-12-11 18:23:25 +0000

    feat(demo control): add var for demo

--- src/routes/(authentication)/login/+page.server.ts
+++ src/routes/(authentication)/login/+page.server.ts
@@ -5,12 +5,16 @@ import { usersTable } from "$lib/db/schema";
 import { eq } from "drizzle-orm";
 import { createCookie } from "$lib/utils/remember_me.js";
 import { ROUTES } from "$lib/constants/routes.js";
+import { WEB_DEMO } from "$env/static/private";
 
 export const load = ({ cookies }) => {
   const cookiesJwt = cookies.get("remember_me");
   if (cookiesJwt) {
     throw redirect(303, ROUTES.landing);
   }
+
+  const webDemo = WEB_DEMO === "true";
+  return { webDemo };
 };
 
 export const actions = {
