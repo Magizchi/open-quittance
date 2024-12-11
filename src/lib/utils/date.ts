bfb147133246ba5cc5378c438c8c39692ecd89ca
Rajanan <g.rajan1993@gmail.com>
2024-12-11 18:12:08 +0000

    fix(util:date): fix import type dayjs

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,25 +1,28 @@
-import dayjs, { Dayjs } from 'dayjs';
-import 'dayjs/locale/fr';
-dayjs.locale('fr');
+import dayjs from "dayjs";
+import "dayjs/locale/fr";
+dayjs.locale("fr");
 
 /**
  * Fonction return month
  * @returns Months [Janvier, février, etc...]
  */
-export const getMonth = (date: string | Date | Dayjs = dayjs()) => {
-	return dayjs(date).locale('fr').format('MMMM').toUpperCase();
+export const getMonth = (date: string | Date | dayjs.ConfigType = dayjs()) => {
+  return dayjs(date).locale("fr").format("MMMM").toUpperCase();
 };
 
 /**
  *
  * @param date
  * @param format default Fr format
- * @returns formate date
+ * @returns default format DD/MM/YYYY
  */
-export const formatDate = (date: string | Date, format: string = 'DD/MM/YYYY') => {
-	return dayjs(date).format(format);
+export const formatDate = (
+  date: string | Date,
+  format: string = "DD/MM/YYYY"
+) => {
+  return dayjs(date).format(format);
 };
 
 export const toDay = () => {
-	return dayjs().toDate();
+  return dayjs().toDate();
 };
