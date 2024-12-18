6901214777fd719636712584907e48cd4e434e85
Rajanan <g.rajan1993@gmail.com>
2024-12-18 15:37:26 +0000

    fix(util:pdfgenerator): add prefix for prod and dev

--- src/lib/utils/PdfGenerator.ts
+++ src/lib/utils/PdfGenerator.ts
@@ -2,19 +2,22 @@ import BlobStream from "blob-stream";
 import type { IBlobStream } from "blob-stream";
 import PdfPrinter from "pdfmake";
 import type { TDocumentDefinitions } from "pdfmake/interfaces";
+import { dev } from "$app/environment";
 
 const GeneratePdf = (docDefinition: TDocumentDefinitions): Promise<Blob> => {
+  const prefix: string = dev ? "static" : "build/client";
+
   const fonts = {
     // Default fonts, don't delete this
     Roboto: {
-      normal: "static/fonts/Roboto/Roboto-Regular.ttf",
-      bold: "static/fonts/Roboto/Roboto-Medium.ttf",
+      normal: `${prefix}/fonts/Roboto/Roboto-Regular.ttf`,
+      bold: `${prefix}/fonts/Roboto/Roboto-Medium.ttf`,
     },
     // Custom fonts
     Hind: {
       // Only Roboto work? I used "Hind"
-      normal: "static/fonts/hind/Hind-Regular.otf",
-      bold: "static/fonts/hind/Hind-Bold.ttf",
+      normal: `${prefix}/fonts/hind/Hind-Regular.otf`,
+      bold: `${prefix}/fonts/hind/Hind-Bold.ttf`,
     },
   };
 
