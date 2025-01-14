3aadb48ef0339a69c130bf846c54cae15837d1ed
Rajanan <g.rajan1993@gmail.com>
2025-01-14 12:38:43 +0000

    style(atoms:clikable): fix cursor important

--- src/lib/components/atoms/Clickable.svelte
+++ src/lib/components/atoms/Clickable.svelte
@@ -57,7 +57,7 @@
     {...rest as ButtonClickableBaseProps}
     {disabled}
     class="px-4 py-2 rounded font-hind w-full cursor-pointer
-    {disabled ? '!bg-gray-400 cursor-not-allowed' : ''}
+    {disabled ? '!bg-gray-400 !cursor-not-allowed' : ''}
       {className}"
     class:defaultClass={variant === "default"}
     class:menuClass={variant === "menu"}
