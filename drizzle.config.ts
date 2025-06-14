c2e4df9416c4e78ff958746fae188e7c71910013
rajanan <g.rajan1993@gmail.com>
2025-06-14 19:26:27 +0000

    chore(config:drizzle): add only url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,6 @@ export default {
   out: "./migrations",
   dialect: "mysql",
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE!,
+    url: process.env.DB_URL!,
   },
 } satisfies Config;
