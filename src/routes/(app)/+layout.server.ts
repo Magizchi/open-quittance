767370a45eda542d8ecba6e0dc082d5e4a63a7ba
rajanan <g.rajan1993@gmail.com>
2025-08-12 08:19:57 +0000

    chore(app:layout):delete unused code

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,23 +1,10 @@
 import { ROUTES } from "$lib/constants/routes";
-import db from "$lib/db/drizzle.js";
-import { landlordsTable } from "$lib/db/schema.js";
 import { redirect } from "@sveltejs/kit";
 
-export const load = async ({ url, locals }) => {
+export const load = async ({ locals }) => {
   if (!locals.session) {
     throw redirect(303, ROUTES.login);
   }
 
-  const landlords = await db.select().from(landlordsTable);
-  let needCreateLandlords = false;
-
-  if (!landlords.length) {
-    needCreateLandlords = true;
-  }
-
-  if (!landlords.length && ROUTES.createLandlords !== url.pathname) {
-    throw redirect(303, ROUTES.createLandlords);
-  }
-
-  return { ...locals.user, needCreateLandlords };
+  return { ...locals.user };
 };
