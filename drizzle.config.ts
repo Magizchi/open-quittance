54a92872b8132fe5be7cf195074a7f8d1656a0f7
rajanan <g.rajan1993@gmail.com>
2025-08-15 12:18:45 +0000

    drizzle use url bdd

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import 'dotenv/config';
+
 import type { Config } from "drizzle-kit";
 
 export default {
@@ -6,10 +7,6 @@ export default {
   out: "./migrations",
   dialect: "mysql",
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE!,
+    url: process.env.DB_URL!
   },
 } satisfies Config;
