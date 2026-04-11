2a9dc23ea9429c335d9d71bcdf3f863ddf78c453
Magizchi <g.rajan1993@gmail.com>
2026-04-11 17:21:33 +0000

    fix pdfmaker

--- src/lib/utils/FormDataToJson.ts
+++ src/lib/utils/FormDataToJson.ts
@@ -1,7 +1,29 @@
 const FormDataToJson = (data: FormData) => {
-	const dataToJson: { [x: string]: string } = {};
+	const dataToJson: Record<string, any> = {};
+
 	for (const key of data.keys()) {
-		dataToJson[key] = data.get(key)!.toString();
+		const value = data.get(key)?.toString() ?? "";
+
+		if (key.includes("[")) {
+			const path = key.split(/\[|\]/).filter((part) => part !== "");
+			let current = dataToJson;
+
+			for (let i = 0; i < path.length; i += 1) {
+				const part = path[i];
+				const isLast = i === path.length - 1;
+
+				if (isLast) {
+					current[part] = value;
+				} else {
+					if (current[part] === undefined || typeof current[part] !== "object") {
+						current[part] = {};
+					}
+					current = current[part];
+				}
+			}
+		} else {
+			dataToJson[key] = value;
+		}
 	}
 
 	return dataToJson;
